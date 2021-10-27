6c82021c70f625f900d7478c1ecc9cf341647bb3
Tommy Chiang <oToToT@users.noreply.github.com>
2021-10-27 22:16:25 +0800

    Fix wrong general matching

--- codes/Matching_and_Flow/GeneralMatching.cpp
+++ codes/Matching_and_Flow/GeneralMatching.cpp
@@ -1,39 +1,68 @@
-const int N = 514, E = (2e5) * 2;
-struct Graph{
-  int to[E],bro[E],head[N],e;
-  int lnk[N],vis[N],stp,n;
-  void init( int _n ){
-    stp = 0; e = 1; n = _n;
-    for( int i = 0 ; i <= n ; i ++ )
-      head[i] = lnk[i] = vis[i] = 0;
+namespace matching {
+int fa[kN], pre[kN], match[kN], s[kN], v[kN];
+vector<int> g[kN];
+queue<int> q;
+void Init(int n) {
+  for (int i = 0; i <= n; ++i) match[i] = pre[i] = n;
+  for (int i = 0; i < n; ++i) g[i].clear();
+}
+void AddEdge(int u, int v) {
+  g[u].push_back(v);
+  g[v].push_back(u);
+}
+int Find(int u) {
+  return u == fa[u] ? u : fa[u] = Find(fa[u]);
+}
+int LCA(int x, int y, int n) {
+  static int tk = 0; tk++;
+  x = Find(x), y = Find(y);
+  for (; ; swap(x, y)) {
+    if (x != n) {
+      if (v[x] == tk) return x;
+      v[x] = tk;
+      x = Find(pre[match[x]]);
+    }
   }
-  void add_edge(int u,int v){
-    // 1-base
-    to[e]=v,bro[e]=head[u],head[u]=e++;
-    to[e]=u,bro[e]=head[v],head[v]=e++;
+}
+void Blossom(int x, int y, int l) {
+  while (Find(x) != l) {
+    pre[x] = y, y = match[x];
+    if (s[y] == 1) q.push(y), s[y] = 0;
+    if (fa[x] == x) fa[x] = l;
+    if (fa[y] == y) fa[y] = l;
+    x = pre[y];
   }
-  bool dfs(int x){
-    vis[x]=stp;
-    for(int i=head[x];i;i=bro[i]){
-      int v=to[i];
-      if(!lnk[v]){
-        lnk[x]=v,lnk[v]=x;
-        return true;
-      }else if(vis[lnk[v]]<stp){
-        int w=lnk[v];
-        lnk[x]=v,lnk[v]=x,lnk[w]=0;
-        if(dfs(w)) return true;
-        lnk[w]=v,lnk[v]=w,lnk[x]=0;
+}
+bool Bfs(int r, int n) {
+  for (int i = 0; i <= n; ++i) fa[i] = i, s[i] = -1;
+  while (!q.empty()) q.pop();
+  q.push(r);
+  s[r] = 0;
+  while (!q.empty()) {
+    int x = q.front(); q.pop();
+    for (int u : g[x]) {
+      if (s[u] == -1) {
+        pre[u] = x, s[u] = 1;
+        if (match[u] == n) {
+          for (int a = u, b = x, last; b != n; a = last, b = pre[a])
+            last = match[b], match[b] = a, match[a] = b;
+          return true;
+        }
+        q.push(match[u]);
+        s[match[u]] = 0;
+      } else if (!s[u] && Find(u) != Find(x)) {
+        int l = LCA(u, x, n);
+        Blossom(x, u, l);
+        Blossom(u, x, l);
       }
     }
-    return false;
   }
-  int solve(){
-    int ans = 0;
-    for(int i=1;i<=n;i++)
-      if(not lnk[i]){
-        stp++; ans += dfs(i);
-      }
-    return ans;
+  return false;
+}
+int Solve(int n) {
+  int res = 0;
+  for (int x = 0; x < n; ++x) {
+    if (match[x] == n) res += Bfs(x, n);
   }
-} graph;
+  return res;
+}}
