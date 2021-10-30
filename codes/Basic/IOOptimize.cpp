be74af564c03e2c7093fd2b37bab358d2f7b97a7
OmeletWithoutEgg <2qbingxuan@gmail.com>
2021-10-30 10:03:52 +0800

    Remove and minimize some sections

--- codes/Basic/IOOptimize.cpp
+++ codes/Basic/IOOptimize.cpp
@@ -1,22 +1,17 @@
 static inline int gc() {
-  static char buf[ 1 << 20 ], *p = buf, *end = buf;
-  if ( p == end ) {
-    end = buf + fread( buf, 1, 1 << 20, stdin );
-    if ( end == buf ) return EOF;
-    p = buf;
-  }
-  return *p++;
+    constexpr int B = 1<<20;
+    static char buf[B], *p, *q;
+    if(p == q &&
+        (q=(p=buf)+fread(buf,1,B,stdin)) == buf)
+      return EOF;
+    return *p++;
 }
 template < typename T >
-static inline bool gn( T &_ ) {
-  register int c = gc(); register T __ = 1; _ = 0;
+static inline bool gn( T &x ) {
+  int c = gc(); T sgn = 1; x = 0;
   while(('0'>c||c>'9') && c!=EOF && c!='-') c = gc();
-  if(c == '-') { __ = -1; c = gc(); }
+  if(c == '-') sgn = -1, c = gc();
   if(c == EOF) return false;
-  while('0'<=c&&c<='9') _ = _ * 10 + c - '0', c = gc();
-  _ *= __;
-  return true;
+  while('0'<=c&&c<='9') x = x*10 + c - '0', c = gc();
+  return x *= sgn, true;
 }
-template < typename T, typename ...Args >
-static inline bool gn( T &x, Args &...args )
-{ return gn(x) && gn(args...); }
--- codes/Data_Structure/ext_pbds.cpp
+++ codes/Data_Structure/ext_pbds.cpp
@@ -1,24 +1,15 @@
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/priority_queue.hpp>
-using __gnu_pbds::pairing_heap_tag;
-using __gnu_pbds::binary_heap_tag;
-using __gnu_pbds::binomial_heap_tag;
-using __gnu_pbds::rc_binomial_heap_tag;
-using __gnu_pbds::thin_heap_tag;
+using namespace __gnu_pbds;
+// heap tags: paring/binary/binomial/rc_binomial/thin
 template<typename T>
-using pbds_heap=__gnu_pbds::prioity_queue<T,less<T>,\
-                                     pairing_heap_tag>;
-// __gnu_pbds::priority_queue<T,less<T>>::point_iterator
+using pbds_heap=__gnu_pbds::prioity_queue<T,less<T>, \
+                                   pairing_heap_tag>;
+// pbds_heap::point_iterator
 // x = pq.push(10); pq.modify(x, 87); a.join(b);
-using __gnu_pbds::rb_tree_tag;
-using __gnu_pbds::ov_tree_tag;
-using __gnu_pbds::splay_tree_tag;
+// tree tags: rb_tree_tag/ov_tree_tag/splay_tree_tag
 template<typename T>
-using ordered_set = __gnu_pbds::tree<T,\
-__gnu_pbds::null_type,less<T>,rb_tree_tag,\
-__gnu_pbds::tree_order_statistics_node_update>;
+using ordered_set = tree<T, null_type, less<T>,
+      rb_tree_tag, tree_order_statistics_node_update>;
 // find_by_order, order_of_key
-template<typename A,typename B>
-using hTable1=__gnu_pbds::cc_hash_table<A,B>;
-template<typename A,typename B>
-using hTable2=__gnu_pbds::gp_hash_table<A,B>;
+// hash tables: cc_hash_table/gp_hash_table
