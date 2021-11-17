f0c0f2010ba333ea218e168787d2cee95326a691
Tommy Chiang <oToToT@users.noreply.github.com>
2021-11-17 21:42:38 +0800

    Add Euler's criterion in QuadraticResidue

--- codes/Math/QuadraticResidue.cpp
+++ codes/Math/QuadraticResidue.cpp
@@ -12,14 +12,15 @@ struct S {
       (x * rhs.y + y * rhs.x) % MOD };
   }
 };
-int64_t get_root(int64_t n, int P) {
-  if (P == 2) return 1;
-  auto check = [&](int64_t x) {
-    return qpow(x, (P - 1) / 2, P); };
-  if (check(n) == P-1) return -1;
-  int64_t a; int w; mt19937 rnd(7122);
-  do { a = rnd() % P;
-    w = ((a * a - n) % P + P) % P;
-  } while (check(w) != P-1);
-  return qpow(S(P, w, a, 1), (P + 1) / 2).x;
+int get_root(int n, int P) {
+	if (P == 2 or n == 0) return n;
+	if (qpow(n, (P - 1) / 2, P) != 1) return -1;
+	auto check = [&](int x) {
+		return qpow(x, (P - 1) / 2, P); };
+	if (check(n) == P-1) return -1;
+	int64_t a; int w; mt19937 rnd(7122);
+	do { a = rnd() % P;
+		w = ((a * a - n) % P + P) % P;
+	} while (check(w) != P - 1);
+	return qpow(S(P, w, a, 1), (P + 1) / 2).x;
 }
