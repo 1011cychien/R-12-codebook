0b7b5719cf9851296f6e100418216b5710ea4341
Tommy Chiang <oToToT@users.noreply.github.com>
2021-12-19 13:29:34 +0800

    Set indent to 2

--- codes/Basic/IOOptimize.cpp
+++ codes/Basic/IOOptimize.cpp
@@ -1,10 +1,10 @@
 static inline int gc() {
-    constexpr int B = 1<<20;
-    static char buf[B], *p, *q;
-    if(p == q &&
-        (q=(p=buf)+fread(buf,1,B,stdin)) == buf)
-      return EOF;
-    return *p++;
+  constexpr int B = 1<<20;
+  static char buf[B], *p, *q;
+  if(p == q &&
+    (q=(p=buf)+fread(buf,1,B,stdin)) == buf)
+    return EOF;
+  return *p++;
 }
 template < typename T >
 static inline bool gn( T &x ) {
