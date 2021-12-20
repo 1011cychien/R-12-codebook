bbbb590505bbcf72d03b088a400d80820821081e
Tommy Chiang <oToToT@users.noreply.github.com>
2021-12-21 03:38:58 +0800

    Minimize BerlekampMassey

--- codes/Math/Berlekamp-Massey.cpp
+++ codes/Math/Berlekamp-Massey.cpp
@@ -1,21 +1,20 @@
 template <typename T>
 vector<T> BerlekampMassey(const vector<T> &output) {
-  std::vector<T> d(output.size() + 1), me, he;
-  size_t fhe = 0;
-  for (size_t i = 1; i <= output.size(); ++i) {
+  vector<T> d(output.size() + 1), me, he;
+  for (size_t f = 0, i = 1; i <= output.size(); ++i) {
     for (size_t j = 0; j < me.size(); ++j)
       d[i] += output[i - j - 2] * me[j];
     if ((d[i] -= output[i - 1]) == 0) continue;
     if (me.empty()) {
-      me.resize(fhe = i);
+      me.resize(f = i);
       continue;
     }
-    std::vector<T> o(i - fhe - 1);
-    T k = -d[i] / d[fhe]; o.push_back(-k);
+    vector<T> o(i - f - 1);
+    T k = -d[i] / d[f]; o.push_back(-k);
     for (T x : he) o.push_back(x * k);
     if (o.size() < me.size()) o.resize(me.size());
     for (size_t j = 0; j < me.size(); ++j) o[j] += me[j];
-    if (i-fhe+he.size() >= me.size()) he = me, fhe = i;
+    if (i-f+he.size() >= me.size()) he = me, f = i;
     me = o;
   }
   return me;
