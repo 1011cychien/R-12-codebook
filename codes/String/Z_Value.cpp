389beb6b3559ea61dc7bc87470a170229108fb1d
Tommy Chiang <oToToT@users.noreply.github.com>
2021-12-25 21:20:57 +0800

    Make Z algorithm readable

--- codes/String/Z_Value.cpp
+++ codes/String/Z_Value.cpp
@@ -1,11 +1,9 @@
-char s[MAXN];
-int len,z[MAXN];
-void Z_value() {
-  int i,j,left,right;
-  z[left=right=0]=len;
-  for(i=1;i<len;i++) {
-    j=max(min(z[i-left],right-i),0);
-    for(;i+j<len&&s[i+j]==s[j];j++);
-    if(i+(z[i]=j)>right)right=i+z[left=i];
+vector<int> Zalgo(const string &s) {
+  vector<int> z(s.size(), s.size());
+  for (int i = 1, l = 0, r = 0; i < z[0]; ++i) {
+    int j = clamp(r - i, 0, z[i - l]);
+    for (; i + j < z[0] and s[i + j] == s[j]; ++j);
+    if (i + (z[i] = j) > r) r = i + z[l = i];
   }
+  return z;
 }
