c5998203d103c4e46cfcf513860a5ee96eb73103
oToToT <ty1208chiang@gmail.com>
2022-02-28 03:13:37 +0800

    Make smallest_rotation shorter

--- codes/String/smallest_rotation.cpp
+++ codes/String/smallest_rotation.cpp
@@ -1,14 +1,11 @@
-string mcp(string s){
+string mcp(string s) {
   int n = s.length();
-  s += s;
-  int i=0, j=1;
-  while (i<n && j<n){
+  s += s; int i = 0, j = 1;
+  while (i < n && j < n) {
     int k = 0;
-    while (k < n && s[i+k] == s[j+k]) k++;
-    if (s[i+k] <= s[j+k]) j += k+1;
-    else i += k+1;
-    if (i == j) j++;
+    while (k < n && s[i + k] == s[j + k]) k++;
+    ((s[i + k] <= s[j + k]) ? j : i) += k + 1;
+    j += (i == j);
   }
-  int ans = i < n ? i : j;
-  return s.substr(ans, n);
+  return s.substr(i < n ? i : j, n);
 }
