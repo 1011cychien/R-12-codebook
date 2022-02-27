2dc163fbe75cbc7a500558ddb27ef2e76ec1cdcd
Tommy Chiang <oToToT@users.noreply.github.com>
2022-02-28 01:41:29 +0800

    Shorten PollardRho

--- codes/Math/PollardRho.cpp
+++ codes/Math/PollardRho.cpp
@@ -2,19 +2,16 @@
 // return any non-trivial factor
 llu pollard_rho(llu n){
   static auto f=[](llu x,llu k,llu m){
-    return add(k,mul(x,x,m),m);
-  };
+    return add(k,mul(x,x,m),m);};
   if (!(n&1)) return 2;
   mt19937 rnd(120821011);
   while(true){
     llu y=2,yy=y,x=rnd()%n,t=1;
-    for(llu sz=2;t==1;sz<<=1) {
-      for(llu i=0;i<sz;++i){
-        if(t!=1)break;
+    for(llu sz=2;t==1;sz<<=1,y=yy) {
+      for(llu i=0;t==1&&i<sz;++i){
         yy=f(yy,x,n);
         t=gcd(yy>y?yy-y:y-yy,n);
       }
-      y=yy;
     }
     if(t!=1&&t!=n) return t;
   }
