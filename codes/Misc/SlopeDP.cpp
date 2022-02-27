6b578cc86f345a518879a3626b478296649bfcb7
oToToT <ty1208chiang@gmail.com>
2022-02-28 03:10:05 +0800

    Make SlopeDP shorter

--- codes/Misc/SlopeDP.cpp
+++ codes/Misc/SlopeDP.cpp
@@ -1,9 +1,9 @@
-struct Line {
+struct L {
   mutable int64_t a, b, p;
-  bool operator<(const Line &rhs) const { return a < rhs.a; }
+  bool operator<(const L &r) const { return a < r.a; }
   bool operator<(int64_t x) const { return p < x; }
 };
-struct DynamicHull : multiset<Line, less<>> {
+struct DynamicHull : multiset<L, less<>> {
   static const int64_t kInf = 1e18;
   bool Isect(iterator x, iterator y) {
     auto Div = [](int64_t a, int64_t b) {
@@ -16,11 +16,12 @@ struct DynamicHull : multiset<Line, less<>> {
   void Insert(int64_t a, int64_t b) {
     auto z = insert({a, b, 0}), y = z++, x = y;
     while (Isect(y, z)) z = erase(z);
-    if (x != begin() && Isect(--x, y)) Isect(x, y = erase(y));
-    while ((y = x) != begin() && (--x)->p >= y->p) Isect(x, erase(y));
+    if (x!=begin()&&Isect(--x,y)) Isect(x, y=erase(y));
+    while ((y = x) != begin() && (--x)->p >= y->p)
+      Isect(x, erase(y));
   }
   int64_t Query(int64_t x) {
     auto l = *lower_bound(x);
-    return l.a * x + l.b; 
+    return l.a * x + l.b;
   }
 };
