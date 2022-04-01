d61f943671541020c31d151dddee666d00be9db6
OmeletWithoutEgg <2qbingxuan@gmail.com>
2022-04-01 14:09:14 +0800

    update tangent about polygon

---
+++ codes/Geometry/convex_hulls_tangent.cpp
@@ -0,0 +1,30 @@
+// RLtangent_PolyPolyC(): get the RL tangent between two convex polygons
+//    Input:  m = number of vertices in polygon 1
+//            V = array of vertices for convex polygon 1 with V[m]=V[0]
+//            n = number of vertices in polygon 2
+//            W = array of vertices for convex polygon 2 with W[n]=W[0]
+//    Output: *t1 = index of tangent point V[t1] for polygon 1
+//            *t2 = index of tangent point W[t2] for polygon 2
+void RLtangent_PolyPolyC(int m, Point *V, int n, Point *W, int *t1, int *t2) {
+  int ix1, ix2; // search indices for polygons 1 and 2
+
+  // first get the initial vertex on each polygon
+  ix1 = Rtangent_PointPolyC(W[0], m, V);   // right tangent from W[0] to V
+  ix2 = Ltangent_PointPolyC(V[ix1], n, W); // left tangent from V[ix1] to W
+
+  // ping-pong linear search until it stabilizes
+  int done = false; // flag when done
+  while (done == false) {
+    done = true; // assume done until...
+    while (ori(W[ix2], V[ix1], V[ix1 + 1]) <= 0) {
+      ++ix1; // get Rtangent from W[ix2] to V
+    }
+    while (ori(V[ix1], W[ix2], W[ix2 - 1]) >= 0) {
+      --ix2;        // get Ltangent from V[ix1] to W
+      done = false; // not done if had to adjust this
+    }
+  }
+  *t1 = ix1;
+  *t2 = ix2;
+  return;
+}
---
+++ codes/Geometry/point_to_convex_hull_tangent.cpp
@@ -0,0 +1,82 @@
+#define above(P, Vi, Vj) (ori(P, Vi, Vj) > 0) // true if Vi is above Vj
+#define below(P, Vi, Vj) (ori(P, Vi, Vj) < 0) // true if Vi is below Vj
+// Rtangent_PointPolyC(): binary search for convex polygon right tangent
+//    Input:  P = a 2D point (exterior to the polygon)
+//            n = number of polygon vertices
+//            V = array of vertices for a 2D convex polygon with V[n] = V[0]
+//    Return: index "i" of rightmost tangent point V[i]
+int Rtangent_PointPolyC(Point P, int n, Point *V) {
+  int a, b, c;
+  int upA, dnC;
+
+  if (below(P, V[1], V[0]) && !above(P, V[n - 1], V[0]))
+    return 0;
+
+  for (a = 0, b = n;;) {
+    c = (a + b) / 2;
+    dnC = below(P, V[c + 1], V[c]);
+    if (dnC && !above(P, V[c - 1], V[c]))
+      return c;
+
+    upA = above(P, V[a + 1], V[a]);
+    if (upA) {
+      if (dnC) {
+        b = c;
+      } else {
+        if (above(P, V[a], V[c]))
+          b = c;
+        else
+          a = c;
+      }
+    } else {
+      if (!dnC) {
+        a = c;
+      } else {
+        if (below(P, V[a], V[c]))
+          b = c;
+        else
+          a = c;
+      }
+    }
+  }
+}
+
+// Ltangent_PointPolyC(): binary search for convex polygon left tangent
+//    Input:  P = a 2D point (exterior to the polygon)
+//            n = number of polygon vertices
+//            V = array of vertices for a 2D convex polygon with V[n]=V[0]
+//    Return: index "i" of leftmost tangent point V[i]
+int Ltangent_PointPolyC(Point P, int n, Point *V) {
+  int a, b, c;
+  int dnA, dnC;
+
+  if (above(P, V[n - 1], V[0]) && !below(P, V[1], V[0]))
+    return 0;
+
+  for (a = 0, b = n;;) {
+    c = (a + b) / 2;
+    dnC = below(P, V[c + 1], V[c]);
+    if (above(P, V[c - 1], V[c]) && !dnC)
+      return c;
+    dnA = below(P, V[a + 1], V[a]);
+    if (dnA) {
+      if (!dnC) {
+        b = c;
+      } else {
+        if (below(P, V[a], V[c]))
+          b = c;
+        else
+          a = c;
+      }
+    } else {
+      if (dnC) {
+        a = c;
+      } else {
+        if (above(P, V[a], V[c]))
+          b = c;
+        else
+          a = c;
+      }
+    }
+  }
+}
