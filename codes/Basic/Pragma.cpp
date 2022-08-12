cb3aa77434ede5dcb40b5f845c4d3192cf03076e
Tommy Chiang <oToToT@users.noreply.github.com>
2022-08-13 02:30:09 +0800

    add mxcsr control to denormaling floating point

--- codes/Basic/Pragma.cpp
+++ codes/Basic/Pragma.cpp
@@ -2,3 +2,4 @@
 #pragma GCC optimize("no-math-errno,unroll-loops")
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4")
 #pragma GCC target("popcnt,abm,mmx,avx,tune=native")
+__builtin_ia32_ldmxcsr(__builtin_ia32_stmxcsr()|0x8000);
