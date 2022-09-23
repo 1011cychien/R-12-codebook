9181d0d946228b7be3cb58fb33e4e8d896d0147d
oToToT <ty1208chiang@gmail.com>
2022-09-24 00:01:17 +0800

    Make BitsetLCS easier to read

--- codes/Misc/BitsetLCS.cpp
+++ codes/Misc/BitsetLCS.cpp
@@ -3,7 +3,7 @@ for (int i = 1, c; i <= n; i++)
   scanf("%d", &c), p[c].set(i);
 for (int i = 1, c; i <= m; i++) {
   scanf("%d", &c), (g = f) |= p[c];
-  f.shift(), f.set(0);
+  f.shiftLeftByOne(), f.set(0);
   ((f = g - f) ^= g) &= g;
 }
 printf("%d\n", f.count());
