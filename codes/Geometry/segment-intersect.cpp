13258cb1bc4ddb025041f09a3d554d2f8261a2a4
OmeletWithoutEgg <2qbingxuan@gmail.com>
2022-09-29 16:51:14 +0800

    Fix ray/segment intersection

--- codes/Geometry/segment-intersect.cpp
+++ codes/Geometry/segment-intersect.cpp
@@ -1,4 +1,4 @@
-struct Segment {
+struct Segment { // closed segment
   PT st, dir; // represent st + t*dir for 0<=t<=1
   Segment(PT s, PT e) : st(s), dir(e - s) {}
   static bool valid(lld p, lld q) {
@@ -6,22 +6,25 @@ struct Segment {
     if (q < 0) q = -q, p = -p;
     return 0 <= p && p <= q;
   }
+  vector<PT> ends() const { return { st, st + dir }; }
 };
-bool isInter(Segment A, PT P) {
-  if (A.dir == PT(0)) return P == A.st;
+template <typename T> bool isInter(T A, PT P) {
+  if (A.dir == PT(0)) return P == A.st; // BE CAREFUL
   return cross(P - A.st, A.dir) == 0 &&
-    Segment::valid(dot(P - A.st, A.dir), norm(A.dir));
+    T::valid(dot(P - A.st, A.dir), norm(A.dir));
 }
 template <typename U, typename V>
 bool isInter(U A, V B) {
-  if (cross(A.dir, B.dir) == 0)
-    return // handle parallel yourself
-      isInter(A, B.st) || isInter(A, B.st+B.dir) ||
-      isInter(B, A.st) || isInter(B, A.st+A.dir);
+  if (cross(A.dir, B.dir) == 0) { // BE CAREFUL
+    bool res = false;
+    for (PT P: A.ends()) res |= isInter(B, P);
+    for (PT P: B.ends()) res |= isInter(A, P);
+    return res;
+  }
   PT D = B.st - A.st;
   lld C = cross(A.dir, B.dir);
   return U::valid(cross(D, B.dir), C) &&
-      V::valid(cross(D, A.dir), C);
+    V::valid(cross(D, A.dir), C);
 }
 struct Line {
   PT st, ed, dir;
