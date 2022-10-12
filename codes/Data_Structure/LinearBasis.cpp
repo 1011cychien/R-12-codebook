2d03156a1d0fe0834803a5c8159dc80ad186f898
oToToT <ty1208chiang@gmail.com>
2022-10-13 01:24:14 +0800

    [LinearBasis] minimize

--- codes/Data_Structure/LinearBasis.cpp
+++ codes/Data_Structure/LinearBasis.cpp
@@ -1,40 +1,29 @@
-const int BITS = 30;
-
-struct Basis {
-    array<pair<int, int>, BITS> b;
-    Basis() { b.fill({0, numeric_limits<int>::min()}); }
-    void add(int x, int p) {
-        for (int i = BITS - 1; i >= 0; i--) if ((x >> i) & 1) {
-            if (b[i].first == 0) {
-                b[i] = {x, p};
-                return;
-            }
-            if (b[i].second < p) {
-                swap(b[i].first, x);
-                swap(b[i].second, p);
-            }
-            x ^= b[i].first;
-        }
+template <int BITS, typename S = int> struct Basis {
+  static constexpr auto MIN = numeric_limits<S>::min();
+  array<pair<uint64_t, S>, BITS> b;
+  Basis() { b.fill({0, MIN}); }
+  void add(uint64_t x, S p) {
+    for (int i = BITS-1; i>=0; i--) if ((x >> i) & 1) {
+      if (b[i].first == 0) return b[i]={x, p}, void();
+      if (b[i].second < p)
+        swap(b[i].first, x), swap(b[i].second, p);
+      x ^= b[i].first;
     }
-    int query_kth(int v, int k) {
-        vector<pair<int,int>> tmp;
-        for (int i = 0; i < BITS; i++) {
-            if (b[i].first == 0) continue;
-            tmp.emplace_back(b[i].first, i);
-        }
-        if (k >= (1 << tmp.size())) return -1;
-        for (int i = int(tmp.size()) - 1; i >= 0; i--) {
-            int j = tmp[i].second;
-            if ((k >> i & 1) ^ (v >> j & 1))
-                v ^= tmp[i].first;
-        }
-        return v;
-    }
-    Basis filter(int l) {
-        Basis res = *this;
-        for (int i = 0; i < BITS; i++)
-            if (res.b[i].second < l)
-                res.b[i] = {0, numeric_limits<int>::min()};
-        return res;
-    }
+  }
+  optional<uint64_t> query_kth(uint64_t v, uint64_t k){
+    vector<pair<uint64_t, int>> o;
+    for (int i = 0; i < BITS; i++)
+      if (b[i].first) o.emplace_back(b[i].first, i);
+    if (k >= (1ULL << o.size())) return {};
+    for (int i = int(o.size()) - 1; i >= 0; i--)
+      if ((k >> i & 1) ^ (v >> o[i].second & 1))
+        v ^= o[i].first;
+    return v;
+  }
+  Basis filter(S l) {
+    Basis res = *this;
+    for (int i = 0; i < BITS; i++)
+      if (res.b[i].second < l) res.b[i] = {0, MIN};
+    return res;
+  }
 };
