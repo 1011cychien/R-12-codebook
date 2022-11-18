5b3490f2c230f144c040c01c0914d6e903076224
Tommy Chiang <oToToT@users.noreply.github.com>
2022-11-18 15:31:50 +0800

    Simplify Point & Hulls Tangent

--- codes/Geometry/point_to_convex_hull_tangent.cpp
+++ codes/Geometry/point_to_convex_hull_tangent.cpp
@@ -6,28 +6,16 @@
 //            V = array of vertices for a 2D convex polygon with V[n] = V[0]
 //    Return: index "i" of rightmost tangent point V[i]
 int Rtangent_PointPolyC(PT P, int n, PT *V) {
-  if (n == 1)
-    return 0;
+  if (n == 1) return 0;
   if (below(P, V[1], V[0]) && !above(P, V[n - 1], V[0]))
     return 0;
-
   for (int a = 0, b = n;;) {
-    int c = (a + b) / 2;
-    int dnC = not above(P, V[c + 1], V[c]);
-    if (dnC && !above(P, V[c - 1], V[c]))
-      return c;
-
-    if (above(P, V[a + 1], V[a])) {
-      if (dnC || above(P, V[a], V[c]))
-        b = c;
-      else
-        a = c;
-    } else {
-      if (!dnC || !below(P, V[a], V[c]))
-        a = c;
-      else
-        b = c;
-    }
+    int c = (a + b) / 2, dnC = not above(P, V[c + 1], V[c]);
+    if (dnC && !above(P, V[c - 1], V[c])) return c;
+    if (above(P, V[a + 1], V[a]))
+      ((dnC || above(P, V[a], V[c])) ? b : a) = c;
+    else
+      ((!dnC || !below(P, V[a], V[c])) ? a : b) = c;
   }
 }
 
@@ -37,25 +25,15 @@ int Rtangent_PointPolyC(PT P, int n, PT *V) {
 //            V = array of vertices for a 2D convex polygon with V[n]=V[0]
 //    Return: index "i" of leftmost tangent point V[i]
 int Ltangent_PointPolyC(PT P, int n, PT *V) {
-  if (n == 1)
-    return 0;
+  if (n == 1) return 0;
   if (above(P, V[n - 1], V[0]) && !below(P, V[1], V[0]))
     return 0;
   for (int a = 0, b = n;;) {
-    int c = (a + b) / 2;
-    int dnC = below(P, V[c + 1], V[c]);
-    if (!below(P, V[c - 1], V[c]) && !dnC)
-      return c;
-    if (below(P, V[a + 1], V[a])) {
-      if (!dnC || below(P, V[a], V[c]))
-        b = c;
-      else
-        a = c;
-    } else {
-      if (dnC || !above(P, V[a], V[c]))
-        a = c;
-      else
-        b = c;
-    }
+    int c = (a + b) / 2, dnC = below(P, V[c + 1], V[c]);
+    if (!below(P, V[c - 1], V[c]) && !dnC) return c;
+    if (below(P, V[a + 1], V[a]))
+      ((!dnC || below(P, V[a], V[c])) ? b : a) = c;
+    else
+      ((dnC || !above(P, V[a], V[c])) ? a : b) = c;
   }
 }
