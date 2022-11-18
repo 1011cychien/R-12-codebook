4f275fbd24b9148ded694181af99fd41c808c893
Tommy Chiang <oToToT@users.noreply.github.com>
2022-11-18 15:43:43 +0800

    Remove gn

--- codes/Basic/IOOptimize.cpp
+++ codes/Basic/IOOptimize.cpp
@@ -6,12 +6,3 @@ static inline int gc() {
     return EOF;
   return *p++;
 }
-template < typename T >
-static inline bool gn( T &x ) {
-  int c = gc(); T sgn = 1; x = 0;
-  while(('0'>c||c>'9') && c!=EOF && c!='-') c = gc();
-  if(c == '-') sgn = -1, c = gc();
-  if(c == EOF) return false;
-  while('0'<=c&&c<='9') x = x*10 + c - '0', c = gc();
-  return x *= sgn, true;
-}
