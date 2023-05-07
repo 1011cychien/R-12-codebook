76b3f83804e89ddbbaf4ae6740097377593ecc8f
OmeletWithoutEgg <2qbingxuan@gmail.com>
2023-05-08 00:02:33 +0800

    Update circle cover style

--- codes/Geometry/CircleCover.cpp
+++ codes/Geometry/CircleCover.cpp
@@ -1,61 +1,59 @@
-const int N = 1021;
-struct CircleCover {
-  int C;
-  Cir c[N];
-  bool g[N][N], overlap[N][N];
-  // Area[i] : area covered by at least i circles
-  double Area[ N ];
-  void init(int _C){ C = _C;}
+#define eb emplace_back
+struct CircleCover { // tested on SPOJ 0.56s N=1000
   struct Teve {
-    PTF p; double ang; int add;
+    PTF p; llf ang; int add;
     Teve() {}
-    Teve(PTF _a, double _b, int _c):p(_a), ang(_b), add(_c){}
-    bool operator<(const Teve &a)const
-    {return ang < a.ang;}
-  }eve[N * 2];
+    Teve(PTF a, llf b, int c) : p(a), ang(b), add(c) {}
+    bool operator<(const Teve &a)
+      const { return ang < a.ang; }
+  };
   // strict: x = 0, otherwise x = -1
-  bool disjuct(Cir &a, Cir &b, int x)
-  {return sign(abs(a.O - b.O) - a.R - b.R) > x;}
+  bool disjunct(Cir &a, Cir &b, int x)
+  { return sgn(abs(a.o - b.o) - a.r - b.r) > x; }
   bool contain(Cir &a, Cir &b, int x)
-  {return sign(a.R - b.R - abs(a.O - b.O)) > x;}
-  bool contain(int i, int j) {
-    /* c[j] is non-strictly in c[i]. */
-    return (sign(c[i].R - c[j].R) > 0 || (sign(c[i].R - c[j].R) == 0 && i < j)) && contain(c[i], c[j], -1);
-  }
-  void solve(){
-    fill_n(Area, C + 2, 0);
-    for(int i = 0; i < C; ++i)
-      for(int j = 0; j < C; ++j)
-        overlap[i][j] = contain(i, j);
-    for(int i = 0; i < C; ++i)
-      for(int j = 0; j < C; ++j)
-        g[i][j] = !(overlap[i][j] || overlap[j][i] ||
-            disjuct(c[i], c[j], -1));
-    for(int i = 0; i < C; ++i){
-      int E = 0, cnt = 1;
-      for(int j = 0; j < C; ++j)
-        if(j != i && overlap[j][i])
-          ++cnt;
-      for(int j = 0; j < C; ++j)
-        if(i != j && g[i][j]) {
-          auto IP = intersectPoint(c[i], c[j]);
-          PTF aa = IP[0], bb = IP[1];
-          llf A = arg(aa-c[i].O), B = arg(bb-c[i].O);
-          eve[E++] = Teve(bb,B,1), eve[E++]=Teve(aa,A,-1);
-          if(B > A) ++cnt;
-        }
-      if(E == 0) Area[cnt] += pi * c[i].R * c[i].R;
-      else{
-        sort(eve, eve + E);
-        eve[E] = eve[0];
-        for(int j = 0; j < E; ++j){
+  { return sgn(a.r - b.r - abs(a.o - b.o)) > x; }
+  vector<llf> solve(vector<Cir> c) {
+    // area[i] : area covered by at least i circles
+    int N = c.size(); vector<llf> area(N + 1);
+    vector<vector<int>> overlap(N, vector<int>(N));
+    auto g = overlap;
+    for (int i = 0; i < N; ++i)
+      for (int j = 0; j < N; ++j) {
+        /* c[j] is non-strictly in c[i]. */
+        overlap[i][j] = i != j &&
+          (sgn(c[i].r - c[j].r) > 0 ||
+           (sgn(c[i].r - c[j].r) == 0 && i < j)) &&
+          contain(c[i], c[j], -1);
+      }
+    for (int i = 0; i < N; ++i)
+      for (int j = 0; j < N; ++j)
+        g[i][j] = i != j && !(overlap[i][j] ||
+            overlap[j][i] || disjunct(c[i], c[j], -1));
+    for (int i = 0; i < N; ++i) {
+      vector<Teve> eve; int cnt = 1;
+      for (int j = 0; j < N;++j)
+        if (overlap[j][i]) ++cnt;
+      // if (cnt > 1) continue; (if only need area[1])
+      for (int j = 0; j < N; ++j) if (g[i][j]) {
+        auto IP = intersectPoint(c[i], c[j]);
+        PTF aa = IP[1], bb = IP[0];
+        llf A = arg(aa - c[i].o), B = arg(bb - c[i].o);
+        eve.eb(bb, B, 1); eve.eb(aa, A, -1);
+        if (B > A) ++cnt;
+      }
+      if (eve.empty())
+        area[cnt] += pi * c[i].r * c[i].r;
+      else {
+        sort(eve.begin(), eve.end());
+        eve.eb(eve[0]); eve.back().ang += 2. * pi;
+        for (size_t j = 0; j + 1 < eve.size(); j++) {
           cnt += eve[j].add;
-          Area[cnt] += cross(eve[j].p, eve[j + 1].p) * .5;
-          double theta = eve[j + 1].ang - eve[j].ang;
-          if (theta < 0) theta += 2. * pi;
-          Area[cnt]+=(theta-sin(theta))*c[i].R*c[i].R*.5;
+          area[cnt] += cross(eve[j].p,eve[j+1].p)*.5;
+          llf t = eve[j + 1].ang - eve[j].ang;
+          area[cnt] += (t-sin(t)) *c[i].r*c[i].r *.5;
         }
       }
     }
+    return area;
   }
-};
+} CCO;
