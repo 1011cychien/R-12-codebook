8fd3ed8b22e79097f95dad9be5ca631780d7952d
OmeletWithoutEgg <2qbingxuan@gmail.com>
2023-05-13 04:14:51 +0800

    Hide hash in TOC and add some fix

--- pdf/hash.sh
+++ pdf/hash.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cpp -dD -P -fpreprocessed "$@" | tr -d '[:space:]' | md5sum | cut -c-6 | tee hash.tmp.txt
+cpp -dD -P -fpreprocessed "$@" | tr -d '[:space:]' | md5sum | cut -c-6 | tee /tmp/hash.tmp.txt
