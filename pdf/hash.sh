5726bdeb88c8b4ff9c55aedd741d6f03fe879d1f
OmeletWithoutEgg <2qbingxuan@gmail.com>
2023-06-11 22:39:17 +0800

    Try fix the hash in automated build again, fix extension

--- pdf/hash.sh
+++ pdf/hash.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mkdir -p `dirname /tmp/hash/pdf/"$@"`
-cpp -dD -P -fpreprocessed "$@" | tr -d '[:space:]' | md5sum | cut -c-6 | tee /tmp/hash/pdf/"$@"
+mkdir -p `dirname /tmp/hash/pdf/"$@".md5`
+cpp -dD -P -fpreprocessed "$@" | tr -d '[:space:]' | md5sum | cut -c-6 | tee /tmp/hash/pdf/"$@".md5
