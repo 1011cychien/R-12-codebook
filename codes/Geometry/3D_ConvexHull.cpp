f0d5342a8232465feed352ec01272e58c3ca606e
OmeletWithoutEgg <2qbingxuan@gmail.com>
2023-06-26 21:41:18 +0800

    Add comments on 3D convex hull

--- codes/Geometry/3D_ConvexHull.cpp
+++ codes/Geometry/3D_ConvexHull.cpp
@@ -1,45 +1,41 @@
 // return the faces with pt indexes
-int flag[MXN][MXN];
-struct Point{
-  ld x,y,z;
-  Point operator * (const ld &b) const {
-    return (Point){x*b,y*b,z*b};}
-  Point operator * (const Point &b) const {
-    return(Point){y*b.z-b.y*z,z*b.x-b.z*x,x*b.y-b.x*y};
+struct P3{
+  lld x,y,z;
+  P3 operator * (const P3 &b) const {
+    return(P3){y*b.z-b.y*z,z*b.x-b.z*x,x*b.y-b.x*y};
   }
 };
-Point ver(Point a, Point b, Point c) {
-  return (b - a) * (c - a);}
-vector<Face> convex_hull_3D(const vector<Point> pt) {
-  int n = SZ(pt), ftop = 0;
-  REP(i,n) REP(j,n) flag[i][j] = 0;
-  vector<Face> now;
-  now.emplace_back(0,1,2);
-  now.emplace_back(2,1,0);
-  for (int i=3; i<n; i++){
-    ftop++; vector<Face> next;
-    REP(j, SZ(now)) {
-      Face& f=now[j]; int ff = 0;
-      ld d=(pt[i]-pt[f.a]).dot(
+struct Face {
+  int a, b, c;
+  Face(int ta,int tb,int tc):a(ta),b(tb),c(tc){}
+};
+P3 ver(P3 a, P3 b, P3 c) { return (b - a) * (c - a); }
+// plz ensure that first 4 points are not coplanar
+// all points coplanar case will WA
+vector<Face> convex_hull_3D(const vector<P3> &pt) {
+  int n = int(pt.size()); vector<Face> now;
+  if (n <= 2) return {}; // be careful about edge case
+  vector<vector<int>> flag(n, vector<int>(n));
+  now.emplace_back(0,1,2); now.emplace_back(2,1,0);
+  for (int i = 3; i < n; i++) {
+    vector<Face> next;
+    for (const auto &f : now) {
+      lld d = (pt[i] - pt[f.a]).dot(
           ver(pt[f.a], pt[f.b], pt[f.c]));
       if (d <= 0) next.push_back(f);
-      if (d > 0) ff=ftop;
-      else if (d < 0) ff=-ftop;
+      int ff = (d > 0) - (d < 0);
       flag[f.a][f.b]=flag[f.b][f.c]=flag[f.c][f.a]=ff;
     }
-    REP(j, SZ(now)) {
-      Face& f=now[j];
-      if (flag[f.a][f.b] > 0 &&
-          flag[f.a][f.b] != flag[f.b][f.a])
-        next.emplace_back(f.a,f.b,i);
-      if (flag[f.b][f.c] > 0 &&
-          flag[f.b][f.c] != flag[f.c][f.b])
-        next.emplace_back(f.b,f.c,i);
-      if (flag[f.c][f.a] > 0 &&
-          flag[f.c][f.a] != flag[f.a][f.c])
-        next.emplace_back(f.c,f.a,i);
+    for (const auto &f : now) {
+      const auto F = [&](int x, int y) {
+        if (flag[x][y] > 0 && flag[y][x] <= 0)
+          next.emplace_back(x, y, i);
+      };
+      F(f.a, f.b); F(f.b, f.c); F(f.c, f.a);
     }
-    now=next;
+    now = next;
   }
   return now;
 }
+// delaunay: facets with negative z normal of
+// convexhull of (x, y, x^2 + y^2)
