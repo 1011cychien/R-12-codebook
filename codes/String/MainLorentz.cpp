a448bf01e88b2cbf06f559993d49d6db0e4171b9
Tommy Chiang <oToToT@users.noreply.github.com>
2023-07-27 14:37:00 +0800

    Make Main-Lorentz useful

--- codes/String/MainLorentz.cpp
+++ codes/String/MainLorentz.cpp
@@ -1,27 +1,33 @@
-vector<tuple<tuple<size_t, size_t, int, int>>> reps;
-void find_repetitions(const string &s, int shift = 0) {
-  if (s.size() <= 1)
-    return;
-  const size_t nu = s.size() / 2, nv = s.size() - nu;
-  string u = s.substr(0, nu), v = s.substr(nu);
-  string ru(u.rbegin(), u.rend());
-  string rv(v.rbegin(), v.rend());
-  find_repetitions(u, shift);
-  find_repetitions(v, shift + nu);
-  auto z1 = Zalgo(ru), z2 = Zalgo(v + '#' + u),
-       z3 = Zalgo(ru + '#' + rv), z4 = Zalgo(v);
-  for (size_t cntr = 0; cntr < s.size(); cntr++) {
-    size_t l; int k1, k2;
+vector<pair<int, int>> rep[kN];
+void main_lorentz(const string &s, int shift = 0) {
+  const int n = s.size();
+  if (n == 1) return;
+  const int nu = n / 2, nv = n - nu;
+  const string u = s.substr(0, nu), v = s.substr(nu),
+        ru(u.rbegin(), u.rend()), rv(v.rbegin(), v.rend());
+  main_lorentz(u, shift), main_lorentz(v, shift + nu);
+  const auto z1 = Zalgo(ru), z2 = Zalgo(v + '#' + u),
+             z3 = Zalgo(ru + '#' + rv), z4 = Zalgo(v);
+  auto get_z = [](const std::vector<int> &z, int i) {
+    return (0 <= i and i < (int)z.size()) ? z[i] : 0; };
+  auto add_rep = [&](bool left, int cntr, int l, int k1, int k2) {
+    const int L = max(1, l - k2), R = std::min(l - left, k1);
+    if (L > R) return;
+    if (left) rep[l].emplace_back(cntr - R, cntr - L);
+    else rep[l].emplace_back(cntr - R - l + 1, cntr - L - l + 1);
+  };
+  for (int cntr = 0; cntr < n; cntr++) {
+    int l, k1, k2;
     if (cntr < nu) {
       l = nu - cntr;
-      k1 = l < z1.size() ? z1[l] : 0;
-      k2 = n + 1 - l < z2.size() ? z2[n + 1 - l] : 0;
+      k1 = get_z(z1, nu - cntr);
+      k2 = get_z(z2, nv + 1 + cntr);
     } else {
       l = cntr - nu + 1;
-      k1 = n + 1 - l < z3.size() ? z3[n + 1 - l] : 0;
-      k2 = l < z4.size() ? z4[l] : 0;
+      k1 = get_z(z3, nu + 1 + nv - 1 - (cntr - nu));
+      k2 = get_z(z4, (cntr - nu) + 1);
     }
     if (k1 + k2 >= l)
-      reps.emplace_back(cntr, l, k1, k2);
+      add_rep(cntr < nu, cntr, l, k1, k2);
   }
 }
