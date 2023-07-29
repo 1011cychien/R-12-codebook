91b29c1d55c614bcb475de4fdec8e250b0291b12
Tommy Chiang <oToToT@users.noreply.github.com>
2023-07-29 21:44:58 +0800

    Fix Main-Lorentz

--- codes/String/MainLorentz.cpp
+++ codes/String/MainLorentz.cpp
@@ -1,20 +1,20 @@
-vector<pair<int, int>> rep[kN];
-void main_lorentz(const string &s, int shift = 0) {
+vector<pair<int, int>> rep[kN]; // 0-base [l, r]
+void main_lorentz(const string &s, int sft = 0) {
   const int n = s.size();
   if (n == 1) return;
   const int nu = n / 2, nv = n - nu;
   const string u = s.substr(0, nu), v = s.substr(nu),
         ru(u.rbegin(), u.rend()), rv(v.rbegin(), v.rend());
-  main_lorentz(u, shift), main_lorentz(v, shift + nu);
+  main_lorentz(u, sft), main_lorentz(v, sft + nu);
   const auto z1 = Zalgo(ru), z2 = Zalgo(v + '#' + u),
              z3 = Zalgo(ru + '#' + rv), z4 = Zalgo(v);
   auto get_z = [](const std::vector<int> &z, int i) {
     return (0 <= i and i < (int)z.size()) ? z[i] : 0; };
-  auto add_rep = [&](bool left, int cntr, int l, int k1, int k2) {
+  auto add_rep = [&](bool left, int c, int l, int k1, int k2) {
     const int L = max(1, l - k2), R = std::min(l - left, k1);
     if (L > R) return;
-    if (left) rep[l].emplace_back(cntr - R, cntr - L);
-    else rep[l].emplace_back(cntr - R - l + 1, cntr - L - l + 1);
+    if (left) rep[l].emplace_back(sft + c - R, sft + c - L);
+    else rep[l].emplace_back(sft + c - R - l + 1, sft + c - L - l + 1);
   };
   for (int cntr = 0; cntr < n; cntr++) {
     int l, k1, k2;
