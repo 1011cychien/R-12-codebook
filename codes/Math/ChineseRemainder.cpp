fee608d79f21a04463ec1c7a079acf30cf6093f8
OmeletWithoutEgg <2qbingxuan@gmail.com>
2023-08-06 04:11:54 +0800

    Fix CRT

--- codes/Math/ChineseRemainder.cpp
+++ codes/Math/ChineseRemainder.cpp
@@ -1,20 +1,10 @@
-lld crt(lld ans[], lld pri[], int n){
-  lld M = 1, ret = 0;
-  for(int i=0;i<n;i++) M *= pri[i];
-  for(int i=0;i<n;i++){
-    lld iv = (gcd(M/pri[i],pri[i]).FF+pri[i])%pri[i];
-    ret += (ans[i]*(M/pri[i])%M * iv)%M;
-    ret %= M;
-  }
-  return ret;
+// please ensure r_i\in[0,m_i)
+bool crt(lld &m1, lld &r1, lld m2, lld r2) {
+    if (m2 > m1) swap(m1, m2), swap(r1, r2);
+    lld g, a, b; exgcd(m1, m2, g, a, b);
+    if ((r2 - r1) % g != 0) return false;
+    m2 /= g; lld D = (r2 - r1) / g % m2 * a % m2;
+    r1 += (D < 0 ? D + m2 : D) * m1; m1 *= m2;
+    assert (r1 >= 0 && r1 < m1);
+    return true;
 }
-/*
-Another:
-x = a1 % m1
-x = a2 % m2
-g = gcd(m1, m2)
-assert((a1-a2)%g==0)
-[p, q] = exgcd(m2/g, m1/g)
-return a2+m2*(p*(a1-a2)/g)
-0 <= x < lcm(m1, m2)
-*/
