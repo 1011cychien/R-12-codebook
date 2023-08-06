5633cb823cd610d2a7cee07cf1b178aa773f785d
OmeletWithoutEgg <2qbingxuan@gmail.com>
2023-08-07 05:57:55 +0800

    Minimize MMC

--- codes/Graph/MinMeanCycle.cpp
+++ codes/Graph/MinMeanCycle.cpp
@@ -1,59 +1,64 @@
-/* minimum mean cycle O(VE) */
-struct MMC{
-#define FZ(n) memset((n),0,sizeof(n))
-#define E 101010
-#define V 1021
-#define inf 1e9
-  struct Edge { int v,u; double c; };
+const int inf = 1e9, V = 1021, E = 101010;
+struct MMC { // O(VE)
+#define FZ(n) memset((n), 0, sizeof(n))
+  struct Edge { int v, u; double c; };
   int n, m, prv[V][V], prve[V][V], vst[V];
   Edge e[E];
   vector<int> edgeID, cycle, rho;
   double d[V][V];
-  void init( int _n ) { n = _n; m = 0; }
+  void init(int _n) {
+    n = _n;
+    m = 0;
+  }
   // WARNING: TYPE matters
-  void add_edge( int vi , int ui , double ci )
-  { e[ m ++ ] = { vi , ui , ci }; }
+  void add_edge(int v, int u, double c) { e[m++] = {v, u, c}; }
   void bellman_ford() {
-    for(int i=0; i<n; i++) d[0][i]=0;
-    for(int i=0; i<n; i++) {
-      fill(d[i+1], d[i+1]+n, inf);
-      for(int j=0; j<m; j++) {
+    for (int i = 0; i < n; i++)
+      d[0][i] = 0;
+    for (int i = 0; i < n; i++) {
+      fill(d[i + 1], d[i + 1] + n, inf);
+      for (int j = 0; j < m; j++) {
         int v = e[j].v, u = e[j].u;
-        if(d[i][v]<inf && d[i+1][u]>d[i][v]+e[j].c) {
-          d[i+1][u] = d[i][v]+e[j].c;
-          prv[i+1][u] = v;
-          prve[i+1][u] = j;
+        if (d[i][v] < inf && d[i + 1][u] > d[i][v] + e[j].c) {
+          d[i + 1][u] = d[i][v] + e[j].c;
+          prv[i + 1][u] = v;
+          prve[i + 1][u] = j;
         }
       }
     }
   }
-  double solve(){
+  double solve() {
     // returns inf if no cycle, mmc otherwise
-    double mmc=inf;
+    double mmc = inf;
     int st = -1;
     bellman_ford();
-    for(int i=0; i<n; i++) {
-      double avg=-inf;
-      for(int k=0; k<n; k++) {
-        if(d[n][i]<inf-eps)
-          avg=max(avg,(d[n][i]-d[k][i])/(n-k));
-        else avg=max(avg,inf);
+    for (int i = 0; i < n; i++) {
+      double avg = -inf;
+      for (int k = 0; k < n; k++) {
+        if (d[n][i] < inf - eps)
+          avg = max(avg, (d[n][i] - d[k][i]) / (n - k));
+        else
+          avg = max(avg, inf);
       }
-      if (avg < mmc) tie(mmc, st) = tie(avg, i);
+      if (avg < mmc)
+        tie(mmc, st) = tie(avg, i);
     }
-    FZ(vst);edgeID.clear();cycle.clear();rho.clear();
-    for (int i=n; !vst[st]; st=prv[i--][st]) {
+    FZ(vst);
+    edgeID.clear();
+    cycle.clear();
+    rho.clear();
+    for (int i = n; !vst[st]; st = prv[i--][st]) {
       vst[st]++;
-      edgeID.PB(prve[i][st]);
-      rho.PB(st);
+      edgeID.emplace_back(prve[i][st]);
+      rho.emplace_back(st);
     }
     while (vst[st] != 2) {
       int v = rho.back(); rho.pop_back();
-      cycle.PB(v);
+      cycle.emplace_back(v);
       vst[v]++;
     }
-    reverse(ALL(edgeID));
-    edgeID.resize(SZ(cycle));
+    reverse(all(edgeID));
+    edgeID.resize(cycle.size());
     return mmc;
   }
 } mmc;
