828781e1eeaa96dc438cbd3d12b46cbe53acecf5
OmeletWithoutEgg <2qbingxuan@gmail.com>
2023-08-07 23:05:39 +0800

    Add comments in farthest pair

--- codes/Geometry/FarthestPair.cpp
+++ codes/Geometry/FarthestPair.cpp
@@ -1,10 +1,11 @@
-// stk is from convex hull
-int n = (int)(stk.size()), pos = 1; lld ans = 0;
+// p is CCW convex hull w/o colinear points
+int n = p.size(), pos = 1; lld ans = 0;
 for (int i = 0; i < n; i++) {
-  P e = stk[(i + 1) % n] - stk[i];
-  while (abs(cross(e, stk[(pos + 1) % n] - stk[i])) >
-         abs(cross(e, stk[pos] - stk[i])))
+  P e = p[(i + 1) % n] - p[i];
+  while (cross(e, p[(pos + 1) % n] - p[i]) >
+         cross(e, p[pos] - p[i]))
     pos = (pos + 1) % n;
-  for (int j: {i, i + 1})
-    ans = max(ans, norm(stk[pos] - stk[j]));
+  for (int j: {i, (i + 1) % n})
+    ans = max(ans, norm(p[pos] - p[j]));
 }
+// tested @ AOJ CGL_4_B
