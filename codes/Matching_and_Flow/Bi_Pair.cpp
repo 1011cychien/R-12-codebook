bfd9057586e193be3ae120a20cac900ed8f7da15
OmeletWithoutEgg <2qbingxuan@gmail.com>
2023-08-08 23:54:40 +0800

    Fix Bi_pair.cpp CE

--- codes/Matching_and_Flow/Bi_Pair.cpp
+++ codes/Matching_and_Flow/Bi_Pair.cpp
@@ -1,7 +1,7 @@
 // G[x] = edges from x. O(V(E+V))
 int solve(vector<vector<int>> &G, int n, int m) {
   vector<int> fX(n, -1), fY(m, -1), vis; int c = 0;
-  const auto dfs = [&](auto self, int x) -> bool {
+  const auto F = [&](auto self, int x) -> bool {
     for (int i : G[x]) if (not vis[i]) {
       vis[i] = true;
       if (fY[i] == -1 || self(self, fY[i]))
@@ -9,6 +9,6 @@ int solve(vector<vector<int>> &G, int n, int m) {
     }
     return false;
   };
-  for (int i=0; i<n; i++) vis.assign(m,0), c+=dfs(i);
+  for (int i=0; i<n; i++) vis.assign(m,0), c+=F(F, i);
   return c;
 }
