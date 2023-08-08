38e00335c836dfe89c9660c6149bc5825a3fe5a3
OmeletWithoutEgg <2qbingxuan@gmail.com>
2023-08-08 23:45:03 +0800

    Minify and change style of data structure

--- codes/Data_Structure/binary_search_on_segtree.cpp
+++ codes/Data_Structure/binary_search_on_segtree.cpp
@@ -4,20 +4,15 @@ template <typename C>
 int find_first(int l, const C &check) {
   if (l >= n) return n + 1;
   l += sz;
-  for (int i = height; i > 0; i--)
-    propagate(l >> i);
+  for (int i = hei; i > 0; i--) propagate(l >> i);
   Monoid sum = identity;
   do {
     while ((l & 1) == 0) l >>= 1;
     if (check(f(sum, data[l]))) {
       while (l < sz) {
-        propagate(l);
-        l <<= 1;
-        auto nxt = f(sum, data[l]);
-        if (not check(nxt)) {
-          sum = nxt;
-          l++;
-        }
+        propagate(l); l <<= 1;
+        if (auto nxt = f(sum,data[l]); not check(nxt))
+          sum = nxt, l++;
       }
       return l + 1 - sz;
     }
@@ -29,21 +24,16 @@ template <typename C>
 int find_last(int r, const C &check) {
   if (r <= 0) return -1;
   r += sz;
-  for (int i = height; i > 0; i--)
-    propagate((r - 1) >> i);
+  for (int i = hei; i > 0; i--) propagate((r-1) >> i);
   Monoid sum = identity;
   do {
     r--;
     while (r > 1 and (r & 1)) r >>= 1;
     if (check(f(data[r], sum))) {
       while (r < sz) {
-        propagate(r);
-        r = (r << 1) + 1;
-        auto nxt = f(data[r], sum);
-        if (not check(nxt)) {
-          sum = nxt;
-          r--;
-        }
+        propagate(r); r = (r << 1) + 1;
+        if (auto nxt = f(data[r],sum); not check(nxt))
+          sum = nxt, r--;
       }
       return r - sz;
     }
