7389b53a1104f52c9b63c748dc0cccbef57453bb
OmeletWithoutEgg <2qbingxuan@gmail.com>
2023-08-08 23:45:16 +0800

    Minify graph section

--- codes/Graph/MinMeanCycle.cpp
+++ codes/Graph/MinMeanCycle.cpp
@@ -1,20 +1,15 @@
 const int inf = 1e9, V = 1021, E = 101010;
 struct MMC { // O(VE)
-#define FZ(n) memset((n), 0, sizeof(n))
-  struct Edge { int v, u; double c; };
+  struct Edge { int v, u; llf c; };
   int n, m, prv[V][V], prve[V][V], vst[V];
   Edge e[E];
   vector<int> edgeID, cycle, rho;
-  double d[V][V];
-  void init(int _n) {
-    n = _n;
-    m = 0;
-  }
+  llf d[V][V];
+  void init(int _n) { n = _n; m = 0; }
   // WARNING: TYPE matters
-  void add_edge(int v, int u, double c) { e[m++] = {v, u, c}; }
+  void add_edge(int v, int u, llf c) { e[m++] = {v, u, c}; }
   void bellman_ford() {
-    for (int i = 0; i < n; i++)
-      d[0][i] = 0;
+    for (int i = 0; i < n; i++) d[0][i] = 0;
     for (int i = 0; i < n; i++) {
       fill(d[i + 1], d[i + 1] + n, inf);
       for (int j = 0; j < m; j++) {
@@ -27,26 +22,22 @@ struct MMC { // O(VE)
       }
     }
   }
-  double solve() {
+  llf solve() {
     // returns inf if no cycle, mmc otherwise
-    double mmc = inf;
+    llf mmc = inf;
     int st = -1;
     bellman_ford();
     for (int i = 0; i < n; i++) {
-      double avg = -inf;
+      llf avg = -inf;
       for (int k = 0; k < n; k++) {
         if (d[n][i] < inf - eps)
-          avg = max(avg, (d[n][i] - d[k][i]) / (n - k));
-        else
-          avg = max(avg, inf);
+          avg = max(avg, (d[n][i]-d[k][i]) / (n-k));
+        else avg = max(avg, inf);
       }
-      if (avg < mmc)
-        tie(mmc, st) = tie(avg, i);
+      if (avg < mmc) tie(mmc, st) = tie(avg, i);
     }
-    FZ(vst);
-    edgeID.clear();
-    cycle.clear();
-    rho.clear();
+    memset(vst, 0, sizeof(vst));
+    edgeID.clear(); cycle.clear(); rho.clear();
     for (int i = n; !vst[st]; st = prv[i--][st]) {
       vst[st]++;
       edgeID.emplace_back(prve[i][st]);
