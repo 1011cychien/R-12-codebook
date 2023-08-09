028c70e90ee1270e336a2da663e17ba0cdb937c6
OmeletWithoutEgg <2qbingxuan@gmail.com>
2023-08-09 18:15:19 +0800

    Fix CE and add comment in Circle/InterWithLine.cpp

--- codes/Geometry/Circle/InterWithLine.cpp
+++ codes/Geometry/Circle/InterWithLine.cpp
@@ -1,8 +1,8 @@
 vector<PTF> LineCircleInter(PTF p1, PTF p2, PTF o, llf r) {
-  PTF ft = p1 + project(c-p1, p2-p1), vec = p2-p1;
-  llf dis = abs(c - ft);
+  PTF ft = p1 + project(o-p1, p2-p1), vec = p2-p1;
+  llf dis = abs(o - ft);
   if (abs(dis - r) < eps) return {ft};
   if (dis > r) return {};
   vec = vec * sqrt(r * r - dis * dis) / abs(vec);
-  return {ft + vec, ft - vec};
+  return {ft + vec, ft - vec}; // sqrt_safe?
 }
