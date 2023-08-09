068ff3af5249bd3e28a5bd2f966b15d2f7c054a5
OmeletWithoutEgg <2qbingxuan@gmail.com>
2023-08-09 22:29:08 +0800

    Further minify and verify MinMeanCycle.cpp

--- codes/Graph/MinMeanCycle.cpp
+++ codes/Graph/MinMeanCycle.cpp
@@ -1,55 +1,39 @@
-const int inf = 1e9, V = 1021, E = 101010;
-struct MMC { // O(VE)
-  struct Edge { int v, u; llf c; };
-  int n, m, prv[V][V], prve[V][V], vst[V];
-  Edge e[E];
-  vector<int> edgeID, cycle, rho;
-  llf d[V][V];
-  void init(int _n) { n = _n; m = 0; }
-  // WARNING: TYPE matters
-  void add_edge(int v, int u, llf c) { e[m++] = {v, u, c}; }
-  void bellman_ford() {
-    for (int i = 0; i < n; i++) d[0][i] = 0;
-    for (int i = 0; i < n; i++) {
-      fill(d[i + 1], d[i + 1] + n, inf);
-      for (int j = 0; j < m; j++) {
-        int v = e[j].v, u = e[j].u;
-        if (d[i][v] < inf && d[i + 1][u] > d[i][v] + e[j].c) {
-          d[i + 1][u] = d[i][v] + e[j].c;
-          prv[i + 1][u] = v;
-          prve[i + 1][u] = j;
-        }
+// WARNING: TYPE matters
+struct Edge { int s, t; llf c; };
+llf solve(vector<Edge> &e, int n) {
+  // O(VE), returns inf if no cycle, mmc otherwise
+  vector<VI> prv(n + 1, VI(n)), prve = prv;
+  vector<vector<llf>> d(n + 1, vector<llf>(n, inf));
+  d[0] = vector<llf>(n, 0);
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < (int)e.size(); j++) {
+      auto [s, t, c] = e[j];
+      if (d[i][s] < inf && d[i + 1][t] > d[i][s] + c) {
+        d[i + 1][t] = d[i][s] + c;
+        prv[i + 1][t] = s; prve[i + 1][t] = j;
       }
     }
   }
-  llf solve() {
-    // returns inf if no cycle, mmc otherwise
-    llf mmc = inf;
-    int st = -1;
-    bellman_ford();
-    for (int i = 0; i < n; i++) {
-      llf avg = -inf;
-      for (int k = 0; k < n; k++) {
-        if (d[n][i] < inf - eps)
-          avg = max(avg, (d[n][i]-d[k][i]) / (n-k));
-        else avg = max(avg, inf);
-      }
-      if (avg < mmc) tie(mmc, st) = tie(avg, i);
+  llf mmc = inf; int st = -1;
+  for (int i = 0; i < n; i++) {
+    llf avg = -inf;
+    for (int k = 0; k < n; k++) {
+      if (d[n][i] < inf - eps)
+        avg = max(avg, (d[n][i] - d[k][i]) / (n - k));
+      else avg = inf;
     }
-    memset(vst, 0, sizeof(vst));
-    edgeID.clear(); cycle.clear(); rho.clear();
-    for (int i = n; !vst[st]; st = prv[i--][st]) {
-      vst[st]++;
-      edgeID.emplace_back(prve[i][st]);
-      rho.emplace_back(st);
-    }
-    while (vst[st] != 2) {
-      int v = rho.back(); rho.pop_back();
-      cycle.emplace_back(v);
-      vst[v]++;
-    }
-    reverse(all(edgeID));
-    edgeID.resize(cycle.size());
-    return mmc;
+    if (avg < mmc) tie(mmc, st) = tie(avg, i);
   }
-} mmc;
+  if (st == -1) return inf;
+  vector<int> vst(n), eid, cycle, rho;
+  for (int i = n; !vst[st]; st = prv[i--][st]) {
+    vst[st]++; eid.emplace_back(prve[i][st]);
+    rho.emplace_back(st);
+  }
+  while (vst[st] != 2) {
+    int v = rho.back(); rho.pop_back();
+    cycle.emplace_back(v); vst[v]++;
+  }
+  reverse(all(eid)); eid.resize(cycle.size());
+  return mmc;
+}
