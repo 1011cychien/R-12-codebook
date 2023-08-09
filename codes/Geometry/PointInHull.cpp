d53c70243bbe8fc13a6ff89a8ece814e0fa7576e
OmeletWithoutEgg <2qbingxuan@gmail.com>
2023-08-09 21:51:25 +0800

    Minify and verify PointInHull.cpp

--- codes/Geometry/PointInHull.cpp
+++ codes/Geometry/PointInHull.cpp
@@ -1,8 +1,7 @@
 bool PIH(const vector<P> &l, P p, bool strict = true) {
   int n = l.size(), a = 1, b = n - 1, r = !strict;
-  if (n < 3)
-    return r && isInter(Seg(l[0], l.back()), p);
-  if (ori(l[0], l[a], l[b]) > 0) swap(a, b);
+  if (n < 3) return r && isInter(Seg(l[0], l[n-1]), p);
+  if (ori(l[0],l[a],l[b]) > 0) swap(a, b);
   if (ori(l[0],l[a],p) >= r || ori(l[0],l[b],p) <= -r)
     return false;
   while (abs(a - b) > 1) {
