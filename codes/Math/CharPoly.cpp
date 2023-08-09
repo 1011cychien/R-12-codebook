7d13d627a3152f2e5711b8bfdb9a72a6bb914ae8
OmeletWithoutEgg <2qbingxuan@gmail.com>
2023-08-09 20:40:42 +0800

    Minify and verify CharPoly.cpp

--- codes/Math/CharPoly.cpp
+++ codes/Math/CharPoly.cpp
@@ -2,16 +2,14 @@
 using VI = vector<int>; using VVI = vector<VI>;
 void Hessenberg(VVI &H, int N) {
   for (int i = 0; i < N - 2; ++i) {
-    if (!H[i + 1][i])
-      for (int j = i + 2; j < N; ++j) if (H[j][i]) {
-        rep(k, i, N) swap(H[i+1][k], H[j][k]);
-        rep(k, 0, N) swap(H[k][i+1], H[k][j]);
-        break;
-      }
+    for (int j = i + 1; j < N; ++j) if (H[j][i]) {
+      rep(k, i, N) swap(H[i+1][k], H[j][k]);
+      rep(k, 0, N) swap(H[k][i+1], H[k][j]);
+      break;
+    }
     if (!H[i + 1][i]) continue;
-    int val = modinv(H[i + 1][i]);
     for (int j = i + 2; j < N; ++j) {
-      int co = mul(val, H[j][i]);
+      int co = mul(modinv(H[i + 1][i]), H[j][i]);
       rep(k, i, N) subeq(H[j][k], mul(H[i+1][k], co));
       rep(k, 0, N) addeq(H[k][i+1], mul(H[k][j], co));
     }
@@ -21,14 +19,13 @@ VI CharacteristicPoly(VVI &A) {
   int N = A.size(); Hessenberg(A, N);
   VVI P(N + 1, VI(N + 1)); P[0][0] = 1;
   for (int i = 1; i <= N; ++i) {
-    P[i][0] = 0;
-    for (int j = 1; j <= i; ++j) P[i][j] = P[i-1][j-1];
+    rep(j, 0, i+1) P[i][j] = j ? P[i-1][j-1] : 0;
     for (int j = i - 1, val = 1; j >= 0; --j) {
-      int co = mul(val, H[j][i - 1]);
+      int co = mul(val, A[j][i - 1]);
       rep(k, 0, j+1) subeq(P[i][k], mul(P[j][k], co));
-      if (j) val = mul(val, H[j][j - 1]);
+      if (j) val = mul(val, A[j][j - 1]);
     }
   }
   if (N & 1) for (int &pi: P[N]) pi = sub(0, pi);
-  return P[N]; // test: 2021 PTZ korea K
+  return P[N]; // test: 2021 PTZ Korea K
 }
