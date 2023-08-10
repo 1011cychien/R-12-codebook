729e2c13ea10950dfc8b0128ac24d4269055d3c6
OmeletWithoutEgg <2qbingxuan@gmail.com>
2023-08-11 06:29:08 +0800

    Minify and fix style and CE of Delaunay.cpp

--- codes/Geometry/Delaunay.cpp
+++ codes/Geometry/Delaunay.cpp
@@ -1,116 +1,87 @@
+bool in_cc(const array<P,3> &p, P q) {
+    i128 det = 0;
+    for (int i = 0; i < 3; i++)
+        det += i128(norm(p[i]) - norm(q)) *
+            cross(p[(i+1)%3] - q, p[(i+2)%3] - q);
+    return det > 0;
+}
 /* Delaunay Triangulation:
 Given a sets of points on 2D plane, find a
 triangulation such that no points will strictly
 inside circumcircle of any triangle.
-find : return a triangle contain given point
+find(root, p) : return a triangle contain given point
 add_point : add a point into triangulation
 A Triangle is in triangulation iff. its has_chd is 0.
-Region of triangle u: iterate each u.edge[i].tri,
+Region of triangle u: iterate each u.e[i].tri,
 each points are u.p[(i+1)%3], u.p[(i+2)%3]
 Voronoi diagram: for each triangle in triangulation,
 the bisector of all its edges will split the region.
 nearest point will belong to the triangle containing it
  */
-const ll inf = MAXC * MAXC * 100; // lower_bound unknown
+#define L(i) ((i)==0 ? 2 : (i)-1)
+#define R(i) ((i)==2 ? 0 : (i)+1)
+#define FOR for (int i = 0; i < 3; i++)
 struct Tri;
-struct Edge {
-  Tri* tri; int side;
-  Edge(): tri(0), side(0){}
-  Edge(Tri* _tri, int _side): tri(_tri), side(_side){}
+struct E {
+  Tri *t; int side; E() : t(0), side(0) {}
+  E(Tri *_t, int _side) : t(_t), side(_side){}
 };
 struct Tri {
-  pll p[3];
-  Edge edge[3];
-  Tri* chd[3];
-  Tri() {}
-  Tri(const pll& p0, const pll& p1, const pll& p2) {
-    p[0] = p0; p[1] = p1; p[2] = p2;
-    chd[0] = chd[1] = chd[2] = 0;
+  array<P,3> p; array<Tri*,3> ch; array<E,3> e;
+  Tri(){} Tri(P a, P b, P c) : p{a, b, c}, ch{} {}
+  bool has_chd() const { return ch[0] != nullptr; }
+  bool contains(P q) const {
+    FOR if (ori(p[i], p[R(i)], q) < 0) return false;
+    return true;
   }
-  bool has_chd() const { return chd[0] != 0; }
-  int num_chd() const {
-    return !!chd[0] + !!chd[1] + !!chd[2];
-  }
-  bool contains(pll const& q) const {
-    for (int i = 0; i < 3; ++i)
-      if (ori(p[i], p[(i + 1) % 3], q) < 0)
-        return 0;
-    return 1;
-  }
-} pool[N * 10], *tris;
-void edge(Edge a, Edge b) {
-  if(a.tri) a.tri->edge[a.side] = b;
-  if(b.tri) b.tri->edge[b.side] = a;
+} pool[maxn * 10], *it;
+void link(E a, E b) {
+  if (a.t) a.t->e[a.side] = b;
+  if (b.t) b.t->e[b.side] = a;
 }
-struct Trig { // Triangulation
-  Trig() {
-    the_root = // Tri should at least contain all points
-      new(tris++) Tri(pll(-inf, -inf), pll(inf + inf, -inf), pll(-inf, inf + inf));
+struct Trigs {
+  Tri *root;
+  Trigs() {
+    root = // should at least contain all points
+      new(it++) Tri(P(-C, -C), P(C*2, -C), P(-C, C*2));
   }
-  Tri* find(pll p) { return find(the_root, p); }
-  void add_point(const pll &p) { add_point(find(the_root, p), p); }
-  Tri* the_root;
-  static Tri* find(Tri* root, const pll &p) {
-    while (1) {
-      if (!root->has_chd())
-        return root;
-      for (int i = 0; i < 3 && root->chd[i]; ++i)
-        if (root->chd[i]->contains(p)) {
-          root = root->chd[i];
-          break;
-        }
-    }
-    assert(0); // "point not found"
+  void add_point(P p) { add_point(find(p, root), p); }
+  static Tri* find(P p, Tri *r) {
+    while (r->has_chd()) for (Tri *c: r->ch)
+        if (c && c->contains(p)) { r = c; break; }
+    return r;
   }
-  void add_point(Tri* root, pll const& p) {
-    Tri* t[3];
+  void add_point(Tri *r, P p) {
+    array<Tri*, 3> t;
     /* split it into three triangles */
-    for (int i = 0; i < 3; ++i)
-      t[i] = new(tris++) Tri(root->p[i], root->p[(i + 1) % 3], p);
-    for (int i = 0; i < 3; ++i)
-      edge(Edge(t[i], 0), Edge(t[(i + 1) % 3], 1));
-    for (int i = 0; i < 3; ++i)
-      edge(Edge(t[i], 2), root->edge[(i + 2) % 3]);
-    for (int i = 0; i < 3; ++i)
-      root->chd[i] = t[i];
-    for (int i = 0; i < 3; ++i)
-      flip(t[i], 2);
+    FOR t[i] = new(it++) Tri(r->p[i], r->p[R(i)], p);
+    FOR link(E(t[i], 0), E(t[R(i)], 1));
+    FOR link(E(t[i], 2), r->e[L(i)]);
+    r->ch = t;
+    FOR flip(t[i], 2);
   }
-  void flip(Tri* tri, int pi) {
-    Tri* trj = tri->edge[pi].tri;
-    int pj = tri->edge[pi].side;
-    if (!trj) return;
-    if (!in_cc(tri->p[0], tri->p[1], tri->p[2], trj->p[pj])) return;
-    /* flip edge between tri,trj */
-    Tri* trk = new(tris++) Tri(tri->p[(pi + 1) % 3], trj->p[pj], tri->p[pi]);
-    Tri* trl = new(tris++) Tri(trj->p[(pj + 1) % 3], tri->p[pi], trj->p[pj]);
-    edge(Edge(trk, 0), Edge(trl, 0));
-    edge(Edge(trk, 1), tri->edge[(pi + 2) % 3]);
-    edge(Edge(trk, 2), trj->edge[(pj + 1) % 3]);
-    edge(Edge(trl, 1), trj->edge[(pj + 2) % 3]);
-    edge(Edge(trl, 2), tri->edge[(pi + 1) % 3]);
-    tri->chd[0] = trk; tri->chd[1] = trl; tri->chd[2] = 0;
-    trj->chd[0] = trk; trj->chd[1] = trl; trj->chd[2] = 0;
-    flip(trk, 1); flip(trk, 2);
-    flip(trl, 1); flip(trl, 2);
+  void flip(Tri* A, int a) {
+    auto [B, b] = A->e[a];
+    if (!B || !in_cc(A->p, B->p[b])) return;
+    /* flip edge between A, B */
+    Tri *X = new(it++)Tri(A->p[R(a)],B->p[b],A->p[a]);
+    Tri *Y = new(it++)Tri(B->p[R(b)],A->p[a],B->p[b]);
+    link(E(X,0), E(Y,0));
+    link(E(X,1), A->e[L(a)]); link(E(X,2), B->e[R(b)]);
+    link(E(Y,1), B->e[L(b)]); link(E(Y,2), A->e[R(a)]);
+    A->ch = B->ch = {X, Y, nullptr};
+    flip(X, 1); flip(X, 2); flip(Y, 1); flip(Y, 2);
   }
 };
-vector<Tri*> triang; // vector of all triangle
-set<Tri*> vst;
-void go(Tri* now) { // store all tri into triang
-  if (vst.find(now) != vst.end())
-    return;
-  vst.insert(now);
-  if (!now->has_chd())
-    return triang.pb(now);
-  for (int i = 0; i < now->num_chd(); ++i)
-    go(now->chd[i]);
+vector<Tri*> res; set<Tri*> vis;
+void go(Tri *now) { // store all tri into res
+  if (!vis.insert(now).second) return;
+  if (!now->has_chd()) return res.push_back(now);
+  for (Tri *c: now->ch) if (c) go(c);
 }
-void build(int n, pll* ps) { // build triangulation
-  tris = pool; triang.clear(); vst.clear();
-  random_shuffle(ps, ps + n);
-  Trig tri; // the triangulation structure
-  for (int i = 0; i < n; ++i)
-    tri.add_point(ps[i]);
-  go(tri.the_root);
+void build(vector<P> &ps) {
+  it = pool; res.clear(); vis.clear();
+  shuffle(ps.begin(), ps.end(), mt19937(114514));
+  Trigs tr; for (P p: ps) tr.add_point(p);
+  go(tr.root); // use `res` afterwards
 }
