642ff4be2ae0421fbbe64abced19c719665bb132
Tommy Chiang <oToToT@users.noreply.github.com>
2023-09-07 00:44:45 +0800

    Remove unnecessary std::

--- codes/String/MainLorentz.cpp
+++ codes/String/MainLorentz.cpp
@@ -8,10 +8,10 @@ void main_lorentz(const string &s, int sft = 0) {
   main_lorentz(u, sft), main_lorentz(v, sft + nu);
   const auto z1 = Zalgo(ru), z2 = Zalgo(v + '#' + u),
              z3 = Zalgo(ru + '#' + rv), z4 = Zalgo(v);
-  auto get_z = [](const std::vector<int> &z, int i) {
+  auto get_z = [](const vector<int> &z, int i) {
     return (0 <= i and i < (int)z.size()) ? z[i] : 0; };
   auto add_rep = [&](bool left, int c, int l, int k1, int k2) {
-    const int L = max(1, l - k2), R = std::min(l - left, k1);
+    const int L = max(1, l - k2), R = min(l - left, k1);
     if (L > R) return;
     if (left) rep[l].emplace_back(sft + c - R, sft + c - L);
     else rep[l].emplace_back(sft + c - R - l + 1, sft + c - L - l + 1);
