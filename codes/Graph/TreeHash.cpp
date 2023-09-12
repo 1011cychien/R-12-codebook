1b4d88a525aca0725a91654cbbcd40ffcad0af6e
OmeletWithoutEgg <2qbingxuan@gmail.com>
2023-09-12 17:04:40 +0800

    Format treehash

--- codes/Graph/TreeHash.cpp
+++ codes/Graph/TreeHash.cpp
@@ -1,8 +1,8 @@
 uint64_t hsah(int u, int f) {
-  uint64_t r = 127;
-  for (int v : G[ u ]) if (v != f) {
+  uint64_t r = 127; // bigger?
+  for (int v : G[u]) if (v != f) {
     uint64_t hh = hsah(v, u);
-    r=(r+(hh*hh)%1010101333)%1011820613;
+    r = (r + (hh * hh) % 1010101333) % 1011820613;
   }
   return r;
 }
