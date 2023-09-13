b1b82a18a96ad3d7fe559b833b2c7de641e16ef6
OmeletWithoutEgg <2qbingxuan@gmail.com>
2023-09-13 16:11:12 +0800

    Make quadratic residue cleaner

--- codes/Math/QuadraticResidue.cpp
+++ codes/Math/QuadraticResidue.cpp
@@ -1,22 +1,18 @@
-struct S {
-  int MOD, w;
-  int64_t x, y;
-  S(int m, int w_=-1, int64_t x_=1, int64_t y_=0)
-    : MOD(m), w(w_), x(x_), y(y_) {}
-  S operator*(const S &rhs) const {
-    return { MOD, rhs.w,
-      (x * rhs.x + y * rhs.y % MOD * rhs.w) % MOD,
-      (x * rhs.y + y * rhs.x) % MOD };
-  }
-};
-int get_root(int n, int P) {
-	if (P == 2 or n == 0) return n;
+int get_root(int n, int P) { // ensure 0 <= n < p
+  if (P == 2 or n == 0) return n;
   auto check = [&](int x) {
-		return qpow(x, (P - 1) / 2, P); };
-	if (check(n) != 1) return -1;
-	int64_t a; int w; mt19937 rnd(7122);
-	do { a = rnd() % P;
-		w = ((a * a - n) % P + P) % P;
-	} while (check(w) != P - 1);
-	return qpow(S(P, w, a, 1), (P + 1) / 2).x;
+    return modpow(x, (P - 1) / 2, P); };
+  if (check(n) != 1) return -1;
+  mt19937 rnd(7122); lld z = 1, w;
+  while (check(w = (z * z - n + P) % P) != P - 1)
+    z = rnd() % P;
+  const auto M = [P, w](auto &u, auto &v) {
+    auto [a, b] = u; auto [c, d] = v;
+    return make_pair((a * c + b * d % P * w) % P,
+        (a * d + b * c) % P);
+  };
+  pair<lld, lld> r(1, 0), e(z, 1);
+  for (int w = (P + 1) / 2; w; w >>= 1, e = M(e, e))
+    if (w & 1) r = M(r, e);
+  return r.first; // sqrt(n) mod P where P is prime
 }
