7baa01a36e313923770cc6a3a932c2b34cd13e25
OmeletWithoutEgg <2qbingxuan@gmail.com>
2023-09-17 21:26:59 +0800

    Add bitset optimization in hopcroft-karp

--- codes/Matching_and_Flow/Bi_Pair_Fast.cpp
+++ codes/Matching_and_Flow/Bi_Pair_Fast.cpp
@@ -2,20 +2,24 @@ struct HK {
   vector<int> l, r, a, p; int ans;
   HK(int n, int m, auto &g) : l(n,-1),r(m,-1),ans(0) {
     for (bool match = true; match; ) {
-      match = false; a.assign(n, -1); p.assign(n, -1);
-      queue<int> q;
+      match = false; a.assign(n, -1); p = a;
+      queue<int> q; int z;
       for (int i = 0; i < n; i++)
         if (l[i] == -1) q.push(a[i] = p[i] = i);
+      // bitset<maxn> nvis, t; nvis.set();
       while (!q.empty()) {
         int x = q.front(); q.pop();
         if (l[a[x]] != -1) continue;
-        for (int y: g[x])
+        // or use _Find_first and _Find_next here
+        for (int y: g[x]) {
+          // nvis.reset(y);
           if (r[y] == -1) {
             while (y != -1)
               r[y] = x, swap(l[x], y), x = p[x];
             match = true; ans++; break;
           } else if (p[r[y]] == -1)
-            q.push(y = r[y]), p[y] = x, a[y] = a[x];
+            q.push(z = r[y]), p[z] = x, a[z] = a[x];
+        }
       }
     }
   }
