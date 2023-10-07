871cd2e0c3314264a52d28fb9d5d906a1e2a2a80
OmeletWithoutEgg <2qbingxuan@gmail.com>
2023-10-08 04:48:06 +0800

    Fix count 3 cycle and 4 cycle

--- codes/Graph/CountCycles.cpp
+++ codes/Graph/CountCycles.cpp
@@ -1,15 +1,13 @@
-sort(ord.begin(), ord.end(), [&](int i, int j) {
-  return pair(deg[i], i) > pair(deg[j], j); });
-for (int i = 0; i < n; i++) rnk[ord[i]] = i;
-if (rnk[u] < rnk[v]) D[u].push_back(v); // D: new DAG
-for (int x = 0; x < n; x++) { // c3
+// ord = sort by deg decreasing, rk[ord[i]] = i
+// D[i] = edge point from rk small to rk big
+for (int x : ord) { // c3
   for (int y : D[x]) vis[y] = 1;
-  for (int y : D[x]) for (int z : D[y]) ans += vis[z];
+  for (int y : D[x]) for (int z : D[y]) c3 += vis[z];
   for (int y : D[x]) vis[y] = 0;
 }
-for (int x = 0; x < n; x++) { // c4
+for (int x : ord) { // c4
   for (int y : D[x]) for (int z : adj[y])
-    if (rnk[z] > rnk[x]) ans += vis[z]++;
+    if (rk[z] > rk[x]) c4 += vis[z]++;
   for (int y : D[x]) for (int z : adj[y])
-    if (rnk[z] > rnk[x]) vis[z]--;
-} // both are O(M * sqrt(M))
+    if (rk[z] > rk[x]) --vis[z];
+} // both are O(M*sqrt(M)), test @ 2022 CCPC guangzhou
