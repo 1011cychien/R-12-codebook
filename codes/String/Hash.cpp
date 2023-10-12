d474a148cd22a758b5bf37326a121ec8c2056d10
Tommy Chiang <oToToT@users.noreply.github.com>
2023-10-13 03:33:49 +0800

    Minimize Hash (w/o test)

--- codes/String/Hash.cpp
+++ codes/String/Hash.cpp
@@ -1,15 +1,14 @@
 class Hash {
-  private:
-    static constexpr int P = 127, Q = 1051762951;
-    vector<int> h, p;
-  public:
-    void init(const string &s){
-      h.assign(s.size()+1, 0); p.resize(s.size()+1);
-      for (size_t i = 0; i < s.size(); ++i)
-        h[i + 1] = add(mul(h[i], P), s[i]);
-      generate(p.begin(), p.end(),[x=1,y=1,this]()
-          mutable{y=x;x=mul(x,P);return y;});
-    }
-    int query(int l, int r){ // 1-base (l, r]
-      return sub(h[r], mul(h[l], p[r-l]));}
+private:
+  static constexpr int P = 127, Q = 1051762951;
+  vector<int> h, p;
+public:
+  Hash(string_view s):h(s.size()+1),p(s.size()+1){
+    for (size_t i = 0; i < s.size(); ++i)
+      h[i + 1] = add(mul(h[i], P), s[i]);
+    generate(p.begin(), p.end(),[x=1,y=1,this]()
+        mutable{y=x;x=mul(x,P);return y;});
+  }
+  int query(int l, int r){ // 1-base (l, r]
+    return sub(h[r], mul(h[l], p[r-l]));}
 };
