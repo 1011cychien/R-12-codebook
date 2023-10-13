b75adfd454c7e48deccb92a8466e08366bee3601
OmeletWithoutEgg <2qbingxuan@gmail.com>
2023-10-14 00:28:18 +0800

    Minify GeneralMatching.cpp

--- codes/FlowAndMatching/GeneralMatching.cpp
+++ codes/FlowAndMatching/GeneralMatching.cpp
@@ -1,68 +1,47 @@
-namespace matching {
-int fa[kN], pre[kN], match[kN], s[kN], v[kN];
-vector<int> g[kN];
-queue<int> q;
-void Init(int n) {
-  for (int i = 0; i <= n; ++i) match[i] = pre[i] = n;
-  for (int i = 0; i < n; ++i) g[i].clear();
-}
-void AddEdge(int u, int v) {
-  g[u].push_back(v);
-  g[v].push_back(u);
-}
-int Find(int u) {
-  return u == fa[u] ? u : fa[u] = Find(fa[u]);
-}
-int LCA(int x, int y, int n) {
-  static int tk = 0; tk++;
-  x = Find(x), y = Find(y);
-  for (; ; swap(x, y)) {
-    if (x != n) {
+
+struct Matching {
+  queue<int> q; int ans, n;
+  vector<int> fa, s, v, pre, match;
+  int Find(int u) {
+    return u == fa[u] ? u : fa[u] = Find(fa[u]); }
+  int LCA(int x, int y, int n) {
+    static int tk = 0; tk++; x = Find(x); y = Find(y);
+    for (;; swap(x, y)) if (x != n) {
       if (v[x] == tk) return x;
       v[x] = tk;
       x = Find(pre[match[x]]);
     }
   }
-}
-void Blossom(int x, int y, int l) {
-  while (Find(x) != l) {
-    pre[x] = y, y = match[x];
-    if (s[y] == 1) q.push(y), s[y] = 0;
-    if (fa[x] == x) fa[x] = l;
-    if (fa[y] == y) fa[y] = l;
-    x = pre[y];
-  }
-}
-bool Bfs(int r, int n) {
-  for (int i = 0; i <= n; ++i) fa[i] = i, s[i] = -1;
-  while (!q.empty()) q.pop();
-  q.push(r);
-  s[r] = 0;
-  while (!q.empty()) {
-    int x = q.front(); q.pop();
-    for (int u : g[x]) {
-      if (s[u] == -1) {
-        pre[u] = x, s[u] = 1;
-        if (match[u] == n) {
-          for (int a = u, b = x, last; b != n; a = last, b = pre[a])
-            last = match[b], match[b] = a, match[a] = b;
-          return true;
-        }
-        q.push(match[u]);
-        s[match[u]] = 0;
-      } else if (!s[u] && Find(u) != Find(x)) {
-        int l = LCA(u, x, n);
-        Blossom(x, u, l);
-        Blossom(u, x, l);
-      }
+  void Blossom(int x, int y, int l) {
+    for (; Find(x) != l; x = pre[y]) {
+      pre[x] = y, y = match[x];
+      if (s[y] == 1) q.push(y), s[y] = 0;
+      for (int z: {x, y}) if (fa[z] == z) fa[z] = l;
     }
   }
-  return false;
-}
-int Solve(int n) {
-  int res = 0;
-  for (int x = 0; x < n; ++x) {
-    if (match[x] == n) res += Bfs(x, n);
+  bool Bfs(auto &&g, int r, int n) {
+    iota(all(fa), 0); ranges::fill(s, -1);
+    q = queue<int>(); q.push(r); s[r] = 0;
+    for (; !q.empty(); q.pop()) {
+      for (int x = q.front(); int u : g[x])
+        if (s[u] == -1) {
+          if (pre[u] = x, s[u] = 1, match[u] == n) {
+            for (int a = u, b = x, last;
+                b != n; a = last, b = pre[a])
+              last = match[b], match[b] = a, match[a] = b;
+            return true;
+          }
+          q.push(match[u]); s[match[u]] = 0;
+        } else if (!s[u] && Find(u) != Find(x)) {
+          int l = LCA(u, x, n);
+          Blossom(x, u, l); Blossom(u, x, l);
+        }
+    }
+    return false;
   }
-  return res;
-}}
+  Matching(auto &&g) : ans(0), n(g.size()), fa(n + 1),
+  s(n + 1), v(n + 1), pre(n + 1, n), match(n + 1, n) {
+    for (int x = 0; x < n; ++x)
+      if (match[x] == n) ans += Bfs(g, x, n);
+  } // match[x] == n means not matched
+}; // test @ yosupo judge
