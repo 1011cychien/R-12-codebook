cce0ff0a4e4ca7bd3f419baf1bc7d0aeeb5d48e3
OmeletWithoutEgg <2qbingxuan@gmail.com>
2023-10-14 13:21:26 +0800

    Update FPS.cpp, add faster linear recurrence

--- codes/Math/FPS.cpp
+++ codes/Math/FPS.cpp
@@ -1,3 +1,4 @@
+#define fi(l, r) for (size_t i = (l); i < (r); ++i)
 using Poly = vector<int>;
 uint32_t n2k(uint32_t n) {
   if (n <= 1) return 1;
@@ -12,7 +13,7 @@ auto Mul(auto a, auto b, size_t sz) {
 Poly Newton(const Poly &v, auto &&init, auto &&iter) {
   Poly Q = { init(v[0]) };
   for (int sz = 2; Q.size() < v.size(); sz *= 2) {
-    Poly A{v.begin(), v.begin() + min(sz, int(v.size()))};
+    Poly A{begin(v), begin(v) + min(sz, int(v.size()))};
     A.resize(sz * 2), Q.resize(sz * 2);
     iter(Q, A, sz * 2); Q.resize(sz);
   }
@@ -20,14 +21,12 @@ Poly Newton(const Poly &v, auto &&init, auto &&iter) {
 }
 Poly Inv(const Poly &v) { // v[0] != 0
   return Newton(v, modinv,
-  [](Poly &X, Poly &A, int sz) {
-    ntt(X.data(), sz), ntt(A.data(), sz);
-    for (int i = 0; i < sz; i++)
-      X[i] = modmul(X[i], modsub(2, modmul(X[i], A[i])));
-    ntt(X.data(), sz, true);
-  });
+    [](Poly &X, Poly &A, int sz) {
+      ntt(X.data(), sz), ntt(A.data(), sz);
+      for (int i = 0; i < sz; i++)
+        X[i] = modmul(X[i], modsub(2, modmul(X[i], A[i])));
+      ntt(X.data(), sz, true); });
 }
-#define fi(l, r) for (size_t i = (l); i < (r); ++i)
 Poly Dx(Poly A) {
   fi(1, A.size()) A[i - 1] = modmul(i, A[i]);
   return A.empty() ? A : (A.pop_back(), A);
@@ -43,11 +42,10 @@ Poly Ln(const Poly &A) { // coef[0] == 1; res[0] == 0
 }
 Poly Exp(const Poly &v) { // coef[0] == 0; res[0] == 1
   return Newton(v, [](int x) { return 1; },
-      [](Poly &X, Poly &A, int sz) {
-        auto Y = X; Y.resize(sz / 2); Y = Ln(Y);
-        fi(0, Y.size()) Y[i] = modsub(A[i], Y[i]);
-        Y[0] = modadd(Y[0], 1); X = Mul(X, Y, sz);
-      });
+    [](Poly &X, Poly &A, int sz) {
+      auto Y = X; Y.resize(sz / 2); Y = Ln(Y);
+      fi(0, Y.size()) Y[i] = modsub(A[i], Y[i]);
+      Y[0] = modadd(Y[0], 1); X = Mul(X, Y, sz); });
 }
 Poly Pow(Poly a, lld M) { // period mod*(mod-1)
   assert(!a.empty() && a[0] != 0);
@@ -61,13 +59,12 @@ Poly Pow(Poly a, lld M) { // period mod*(mod-1)
 Poly Sqrt(const Poly &v) { // need: QuadraticResidue
   assert(!v.empty() && v[0] != 0);
   const int r = get_root(v[0]); assert(r != -1);
-  return Newton(u, [r](int x) { return r; },
+  return Newton(v, [r](int x) { return r; },
     [](Poly &X, Poly &A, int sz) {
       auto Y = X; Y.resize(sz / 2);
       auto B = Mul(A, Inv(Y), sz);
-      for (int i = 0; i < sz; i++)
-        X[i] = modmul(mod / 2 + 1, modadd(X[i], B[i]));
-    });
+      for (int i = 0, inv2 = mod / 2 + 1; i < sz; i++)
+        X[i] = modmul(inv2, modadd(X[i], B[i])); });
 }
 Poly Mul(auto &&a, auto &&b) {
   const int n = a.size() + b.size() - 1;
@@ -108,13 +105,21 @@ pair<Poly, Poly> DivMod(const Poly &A, const Poly &B) {
   while (Q.size() && Q.back() == 0) Q.pop_back();
   return {Q, Y};
 } // empty means zero polynomial
-int LinearRecursion(Poly a, Poly c, int64_t n) {
-  const auto k = a.size(); assert(c.size() == k + 1);
-  Poly p(k + 1), r(k), e = {0, 1}; r[0] = 1; p[k] = 1;
-  fi(1, k + 1) p[k - i] = modsub(0, c[i]);
-  for (; n; n >>= 1, e = DivMod(Mul(e, e), p).second)
-    if (n & 1) r = DivMod(Mul(r, e), p).second;
-  int ret = 0; r.resize(k);
-  fi(0, k) ret = modadd(ret, modmul(r[i], a[i]));
-  return ret;
+int LinearRecursionKth(Poly a, Poly c, int64_t k) {
+  const auto d = a.size(); assert(c.size() == d + 1);
+  const int sz = n2k(2 * d + 1), o = sz / 2;
+  Poly q = c; for (int &x: q) x = modsub(0, x); q[0]=1;
+  Poly p = Mul(a, q); p.resize(sz); q.resize(sz);
+  for (int r; r = (k & 1), k; k >>= 1) {
+    fill(d + all(p), 0); fill(d + 1 + all(q), 0);
+    ntt(p.data(), sz); ntt(q.data(), sz);
+    for(int i = 0; i < sz; i++)
+      p[i] = modmul(p[i], q[(i + o) & (sz - 1)]);
+    for(int i = 0, j = o; j < sz; i++, j++)
+      q[i] = q[j] = modmul(q[i], q[j]);
+    ntt(p.data(), sz, true); ntt(q.data(), sz, true);
+    for (int i = 0; i < d; i++) p[i] = p[i << 1 | r];
+    for (int i = 0; i <= d; i++) q[i] = q[i << 1];
+  } // Bostan-Mori
+  return modmul(p[0], modinv(q[0]));
 } // a_n = \sum c_j a_(n-j), c_0 is not important
