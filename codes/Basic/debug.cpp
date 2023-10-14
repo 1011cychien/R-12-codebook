2a171b75961ca55c3a305e0ff9d93e5c597f3679
OmeletWithoutEgg <2qbingxuan@gmail.com>
2023-10-14 19:56:26 +0800

    Fix debug.cpp

--- codes/Basic/debug.cpp
+++ codes/Basic/debug.cpp
@@ -1,25 +1,21 @@
+#include <experimental/iterator>
 #define all(x) begin(x), end(x)
-#ifdef local
+#ifdef CKISEKI
 #define safe cerr<<__PRETTY_FUNCTION__<<" line "<<__LINE__<<" safe\n"
-#define debug(a...) \
-  cerr<<"\e[1;32m("#a") = ("<<debug_(a)<<")\e[0m\n"
-#define orange(a...) \
-  orange_("\e[1;32m[ "#a" ] = [ ", a, " ]\e[0m\n")
-template <typename T>
-string debug_(T t) { return to_string(t); }
-template <typename U, typename ...V>
-string debug_(U u, V ...v) {
-  return to_string(u) + ", " + debug_(v...);
+#define debug(a...) cerr<<"\e[1;32m("#a") = ("<<debug_(a)<<")\e[0m\n"
+#define orange(a...) cerr<<"\e[1;32m[ "#a" ] = [ "<<orange_(a)<<" ]\e[0m\n"
+template <typename ...T> string debug_(T ...a) {
+  stringstream ss; int f = 0;
+  (..., (ss << (f++ ? ", " : "") << a));
+  return ss.str();
 }
-template <typename I>
-void orange_(string pre, I L, I R, string post) {
-  cerr << pre;
-  copy(L, R, ostream_iterator<decltype(*L)>(cerr, ", "));
-  cerr << post;
+template <typename I> string orange_(I L, I R) {
+  stringstream ss; using namespace std::experimental;
+  copy(L, R, make_ostream_joiner(ss, ", "));
+  return ss.str();
 }
 #else
 #define safe ((void)0)
 #define debug(...) safe
 #define orange(...) safe
 #endif
-
