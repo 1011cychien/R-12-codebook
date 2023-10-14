6faadd658a9d54e3845988f4a4a38326b8419bd2
OmeletWithoutEgg <2qbingxuan@gmail.com>
2023-10-14 14:14:01 +0800

    Update debug.cpp

--- codes/Basic/debug.cpp
+++ codes/Basic/debug.cpp
@@ -1,23 +1,25 @@
 #define all(x) begin(x), end(x)
-#ifdef CKISEKI
+#ifdef local
 #define safe cerr<<__PRETTY_FUNCTION__<<" line "<<__LINE__<<" safe\n"
-#define debug(a...) debug_(#a, a)
-#define orange(a...) orange_(#a, a)
-template <typename ...T>
-void debug_(const char *s, T ...a) {
-    cerr << "\e[1;32m(" << s << ") = (";
-    int cnt = sizeof...(T);
-    (..., (cerr << a << (--cnt ? ", " : ")\e[0m\n")));
+#define debug(a...) \
+  cerr<<"\e[1;32m("#a") = ("<<debug_(a)<<")\e[0m\n"
+#define orange(a...) \
+  orange_("\e[1;32m[ "#a" ] = [ ", a, " ]\e[0m\n")
+template <typename T>
+string debug_(T t) { return to_string(t); }
+template <typename U, typename ...V>
+string debug_(U u, V ...v) {
+  return to_string(u) + ", " + debug_(v...);
 }
 template <typename I>
-void orange_(const char *s, I L, I R) {
-    cerr << "\e[1;32m[ " << s << " ] = [ ";
-    for (int f = 0; L != R; ++L)
-        cerr << (f++ ? ", " : "") << *L;
-    cerr << " ]\e[0m\n";
+void orange_(string pre, I L, I R, string post) {
+  cerr << pre;
+  copy(L, R, ostream_iterator<decltype(*L)>(cerr, ", "));
+  cerr << post;
 }
 #else
 #define safe ((void)0)
 #define debug(...) safe
 #define orange(...) safe
 #endif
+
