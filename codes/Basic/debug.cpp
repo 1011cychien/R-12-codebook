ab28837d5d109a1586fb2d3c8977b568b2b7c60c
OmeletWithoutEgg <2qbingxuan@gmail.com>
2023-10-16 00:54:42 +0800

    Update debug.cpp

--- codes/Basic/debug.cpp
+++ codes/Basic/debug.cpp
@@ -1,18 +1,18 @@
-#include <experimental/iterator>
 #define all(x) begin(x), end(x)
 #ifdef CKISEKI
+#include <experimental/iterator>
 #define safe cerr<<__PRETTY_FUNCTION__<<" line "<<__LINE__<<" safe\n"
 #define debug(a...) cerr<<"\e[1;32m("#a") = ("<<debug_(a)<<")\e[0m\n"
-#define orange(a...) cerr<<"\e[1;32m[ "#a" ] = [ "<<orange_(a)<<" ]\e[0m\n"
-template <typename ...T> string debug_(T ...a) {
+#define orange(a...) cerr<<"\e[1;33m[ "#a" ] = [ "<<orange_(a)<<" ]\e[0m\n"
+auto debug_(auto ...a) {
   stringstream ss; int f = 0;
   (..., (ss << (f++ ? ", " : "") << a));
   return ss.str();
 }
-template <typename I> string orange_(I L, I R) {
+auto orange_(auto L, auto R) {
   stringstream ss; using namespace std::experimental;
   copy(L, R, make_ostream_joiner(ss, ", "));
-  return ss.str();
+  return ss.str(); // or ostream_iterator<decltype(*L)>
 }
 #else
 #define safe ((void)0)
