7bfae782064a8354f0a53f1070180f1d1cc9cd43
Tommy Chiang <oToToT@users.noreply.github.com>
2023-10-15 09:07:07 +0800

    make FastDirectedMST shorter

--- codes/Graph/FastDirectedMST.cpp
+++ codes/Graph/FastDirectedMST.cpp
@@ -3,19 +3,14 @@ struct E { int s, t; D w; }; // 0-base
 vector<int> dmst(const vector<E> &e, int n, int root) {
   using PQ = pair<min_heap<pair<D, int>>, D>;
   auto push = [](PQ &pq, pair<D, int> v) {
-    pq.first.emplace(v.first - pq.second, v.second);
-  };
+    pq.first.emplace(v.first - pq.second, v.second); };
   auto top = [](const PQ &pq) -> pair<D, int> {
     auto r = pq.first.top();
-    return {r.first + pq.second, r.second};
-  };
+    return {r.first + pq.second, r.second}; };
   auto join = [&push, &top](PQ &a, PQ &b) {
     if (a.first.size() < b.first.size()) swap(a, b);
-    while (!b.first.empty()) {
-      push(a, top(b));
-      b.first.pop();
-    }
-  };
+    for (; !b.first.empty(); b.first.pop())
+      push(a, top(b)); };
   vector<PQ> h(n * 2);
   for (size_t i = 0; i < e.size(); ++i)
     push(h[e[i].t], {e[i].w, i});
@@ -25,28 +20,20 @@ vector<int> dmst(const vector<E> &e, int n, int root) {
     for (y = x; a[y] != y; y = a[y]);
     for (int ox = x; x != y; ox = x)
       x = a[x], a[ox] = y;
-    return y;
-  };
-  v[root] = n + 1;
-  int pc = n;
-  for (int i = 0; i < n; ++i) if (v[i] == -1) {
-    for (int p = i; v[p] == -1 || v[p] == i; p = o(e[r[p]].s)) {
-      if (v[p] == i) {
-        int q = p; p = pc++;
-        do {
-          h[q].second = -h[q].first.top().first;
-          join(h[pa[q] = a[q] = p], h[q]);
-        } while ((q = o(e[r[q]].s)) != p);
-      }
-      v[p] = i;
-      while (!h[p].first.empty() && o(e[top(h[p]).second].s) == p)
-        h[p].first.pop();
+    return y; };
+  int pc = (v[root] = n + 1) - 1;
+  for (int i = 0; i < n; ++i) if (v[i] == -1)
+    for (int p=i; v[p]<0||v[p]==i; p=o(e[r[p]].s)) {
+      if (int q = p; v[q] == i && (p = pc++, 1)) do {
+        h[q].second = -h[q].first.top().first;
+        join(h[pa[q] = a[q] = p], h[q]);
+      } while ((q = o(e[r[q]].s)) != p);
+      for(v[p]=i;!h[p].first.empty()&&o(e[top(h[p]).second].s)==p;h[p].first.pop());
       r[p] = top(h[p]).second;
     }
-  }
   vector<int> ans;
-  for (int i = pc - 1; i >= 0; i--) if (i != root && v[i] != n) {
-    for (int f = e[r[i]].t; f != -1 && v[f] != n; f = pa[f])
+  for (int i=pc-1;i>=0;i--) if (i!=root&&v[i]!=n) {
+    for (int f = e[r[i]].t; f!=-1&&v[f]!=n; f = pa[f])
       v[f] = n;
     ans.push_back(r[i]);
   }
