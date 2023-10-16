ed75d8eb8bc4f2a2b24c152e317bd58e38c5b2e8
Tommy Chiang <oToToT@users.noreply.github.com>
2023-10-16 22:00:08 +0800

    adjust debug.cpp

--- codes/Basic/debug.cpp
+++ codes/Basic/debug.cpp
@@ -2,17 +2,17 @@
 #ifdef CKISEKI
 #include <experimental/iterator>
 #define safe cerr<<__PRETTY_FUNCTION__<<" line "<<__LINE__<<" safe\n"
-#define debug(a...) cerr<<"\e[1;32m("#a") = ("<<debug_(a)<<")\e[0m\n"
-#define orange(a...) cerr<<"\e[1;33m[ "#a" ] = [ "<<orange_(a)<<" ]\e[0m\n"
-auto debug_(auto ...a) {
-  stringstream ss; int f = 0;
+#define debug(a...) cerr<<"\e[1;32m("#a") = (",debug_(cerr,a)<<")\e[0m\n"
+#define orange(a...) cerr<<"\e[1;33m[ "#a" ] = [ ",orange_(cerr,a)<<" ]\e[0m\n"
+auto &debug_(auto &ss, auto ...a) {
+  int f = 0;
   (..., (ss << (f++ ? ", " : "") << a));
-  return ss.str();
+  return ss;
 }
-auto orange_(auto L, auto R) {
-  stringstream ss; using namespace std::experimental;
+auto &orange_(auto &ss, auto L, auto R) {
+  using namespace experimental;
   copy(L, R, make_ostream_joiner(ss, ", "));
-  return ss.str(); // or ostream_iterator<decltype(*L)>
+  return ss; // or ostream_iterator<decltype(*L)>
 }
 #else
 #define safe ((void)0)
