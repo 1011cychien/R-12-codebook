07e885e334dbb9370fa9af05ae406e517d9b9340
OmeletWithoutEgg <2qbingxuan@gmail.com>
2023-10-17 10:37:05 +0800

    Avoid wrapped line of MinPlusConv.cpp

--- codes/Misc/MinPlusConv.cpp
+++ codes/Misc/MinPlusConv.cpp
@@ -6,7 +6,7 @@ vector<int> min_plus_convolution(auto &a, auto &b) {
     int mid = (l + r) / 2, from = -1, &best = c[mid];
     for (int j = jl; j <= jr; j++)
       if (int i = mid - j; i >= 0 && i < n)
-        if (best > a[i] + b[j]) best = a[i] + b[j], from = j;
+        if (best > a[i]+b[j]) best = a[i]+b[j], from = j;
     Y(Y, l, mid-1, jl, from); Y(Y, mid+1, r, from, jr);
   };
   return dc(dc, 0, n-1+m-1, 0, m-1), c;
