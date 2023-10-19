84e6debfdbf35f2a460306154c2aa61113328fee
OmeletWithoutEgg <2qbingxuan@gmail.com>
2023-10-19 23:17:32 +0800

    Update SVGWriter.cpp

--- codes/Basic/SVGWriter.cpp
+++ codes/Basic/SVGWriter.cpp
@@ -1,16 +1,28 @@
 struct SVG {
-  ofstream o;
-  SVG(string fn, auto x1, auto y1, auto x2, auto y2) : o(fn) {
-    o << "<svg xmlns='http://www.w3.org/2000/svg' viewBox='" << x1 << " " << -y2 << " " << x2 - x1 << " " << y2 - y1 << "'>\n";
+  ofstream o; string c = "red";
+  SVG(auto s, auto x1, auto y1, auto x2, auto y2):o(s){
+    print("<svg xmlns='http://www.w3.org/2000/svg' "
+        "viewBox='% % % %'>\n", x1, -y2, x2 - x1, y2 - y1);
+  }
+  void print(auto f) { o << f; }
+  void print(const char *f, auto value, auto ...rest) {
+    for (; *f; ++f) if (*f == '%') {
+      o << value; print(f + 1, rest...); return;
+    } else o << *f;
   }
   ~SVG() { o << "</svg>" << endl; }
-  void line(auto x1, auto y1, auto x2, auto y2, string c = "red") {
-    o << "<line x1='" << x1 << "' y1='" << -y1 << "' x2='" << x2 << "' y2='" << -y2 << "' stroke='" + c + "'/>\n";
+  SVG &color(string nc) { return c = nc, *this; }
+  void line(auto x1, auto y1, auto x2, auto y2) {
+    print("<line stroke='%' stroke-width='%' "
+        "x1='%' y1='%' x2='%' y2='%'/>\n",
+        c, 0.1, x1, -y1, x2, -y2);
   }
-  void circle(auto x, auto y, auto r, string c = "red") {
-    o << "<circle cx='" << x << "' cy='" << -y << "' r='" << r << "' stroke='" + c + "' fill='none'/>\n";
+  void circle(auto x, auto y, auto r) {
+    print("<circle stroke='%' fill='none' "
+        "cx='%' cy='%' r='%'/>\n", c, x, -y, r);
   }
   void text(auto x, auto y, string s, int w = 12) {
-    o << "<text x='" << x << "' y='" << -y << "' font-size='" << w << "px'>" + s + "</text>\n";
+    print("<text x='%' y='%' font-size='%px'>%</text>",
+        x, -y, w, s);
   }
 };
