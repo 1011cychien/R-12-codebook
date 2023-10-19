05b74b65d943452cf57567a32f08dca01c59ec4e
Tommy Chiang <ty1208chiang@gmail.com>
2023-10-20 01:12:51 +0800

    short SVGWriter

--- codes/Basic/SVGWriter.cpp
+++ codes/Basic/SVGWriter.cpp
@@ -1,28 +1,24 @@
-struct SVG {
+class SVG {
+  void p(string_view fmt) { o << fmt; }
+  void p(string_view fmt, auto v, auto... vs) {
+    auto p = fmt.find('$');
+    o<<fmt.substr(0, p)<<v,p(fmt.substr(p+1),vs...);
+  }
   ofstream o; string c = "red";
-  SVG(auto s, auto x1, auto y1, auto x2, auto y2):o(s){
-    print("<svg xmlns='http://www.w3.org/2000/svg' "
-        "viewBox='% % % %'>\n", x1, -y2, x2 - x1, y2 - y1);
-  }
-  void print(auto f) { o << f; }
-  void print(const char *f, auto value, auto ...rest) {
-    for (; *f; ++f) if (*f == '%') {
-      o << value; print(f + 1, rest...); return;
-    } else o << *f;
-  }
-  ~SVG() { o << "</svg>" << endl; }
-  SVG &color(string nc) { return c = nc, *this; }
+public:
+  SVG(auto f,auto x1,auto y1,auto x2,auto y2) : o(f) {
+    p("<svg xmlns='http://www.w3.org/2000/svg' "
+      "viewBox='$ $ $ $'>\n"
+      "<style>*{stroke-width:0.5%;}</style>\n",
+      x1, -y2, x2 - x1, y2 - y1); }
+  ~SVG() { p("</svg>\n"); }
   void line(auto x1, auto y1, auto x2, auto y2) {
-    print("<line stroke='%' stroke-width='%' "
-        "x1='%' y1='%' x2='%' y2='%'/>\n",
-        c, 0.1, x1, -y1, x2, -y2);
-  }
+    p("<line x1='$' y1='$' x2='$' y2='$' stroke='$'/>\n",
+      x1, -y1, x2, -y2, c); }
   void circle(auto x, auto y, auto r) {
-    print("<circle stroke='%' fill='none' "
-        "cx='%' cy='%' r='%'/>\n", c, x, -y, r);
-  }
+    p("<circle cx='$' cy='$' r='$' stroke='$' "
+      "fill='none'/>\n", x, -y, r, c); }
   void text(auto x, auto y, string s, int w = 12) {
-    print("<text x='%' y='%' font-size='%px'>%</text>",
-        x, -y, w, s);
-  }
+    p("<text x='$' y='$' font-size='$px'>$</text>\n",
+      x, -y, w, s); }
 };
