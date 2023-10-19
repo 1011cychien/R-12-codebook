bac066e16a1906694da9034e8c8ff172d2885375
OmeletWithoutEgg <2qbingxuan@gmail.com>
2023-10-19 23:54:53 +0800

    Add spherical coordinate

--- codes/Geometry/3d/Basic.cpp
+++ codes/Geometry/3d/Basic.cpp
@@ -3,6 +3,10 @@ struct P3 {
   P3 operator^(const P3 &b) const {
     return {y*b.z-b.y*z, z*b.x-b.z*x, x*b.y-b.x*y};
   }
+  //Azimuthal angle (longitude) to x-axis. \in [-pi, pi]
+	llf phi() const { return atan2(y, x); }
+	//Zenith angle (latitude) to the z-axis. \in [0, pi]
+	llf theta() const { return atan2(sqrt(x*x+y*y),z); }
 };
 P3 ver(P3 a, P3 b, P3 c) { return (b - a) ^ (c - a); }
 lld volume(P3 a, P3 b, P3 c, P3 d) {
