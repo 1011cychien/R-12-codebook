33b8a279839b01026d86436ca2cbaefafc78d045
OmeletWithoutEgg <2qbingxuan@gmail.com>
2023-10-20 23:06:56 +0800

    Update RotatingSweepLine.cpp

--- codes/Geometry/RotatingSweepLine.cpp
+++ codes/Geometry/RotatingSweepLine.cpp
@@ -1,23 +1,39 @@
-void rotatingSweepLine(P a[], int n) {
-  vector<pair<int,int>> ls; ls.reserve(n*(n-1)/2);
-  for (int i = 0; i < n; ++i)
-    for (int j = i + 1; j < n; ++j)
-      ls.emplace_back(i, j);
-  sort(all(ls), [&a](auto &u, auto &v){
-    P zu = a[u.first] - a[u.second];
-    P zv = a[v.first] - a[v.second];
-    int s = sgn(RE(zu)) * sgn(RE(zv));
-    if (s == 0) return RE(zu) != 0;
-    return sgn(cross(zu, zv)) * s > 0;
-  });
-  vector<int> idx(n), p(n);
-  iota(all(idx), 0);
-  sort(all(idx), [&a](int i, int j) {
-    return cmpxy(a[i], a[j]); });
-  for (int i = 0; i < n; ++i) p[idx[i]] = i;
-  for (auto [i, j]: ls) {
-    // do here
-    assert (abs(p[i] - p[j]) == 1);
-    swap(p[i], p[j]); idx[p[i]] = i; idx[p[j]] = j;
-  } // consider swap same slope together?
+struct Event {
+  P d; int u, v;
+  bool operator<(const Event &b) const {
+    return sgn(cross(d, b.d)) > 0; }
+};
+P makePositive(P z) { return cmpxy(z, 0) ? -z : z; }
+void rotatingSweepLine(const vector<P> &p) {
+  const int n = int(p.size());
+  vector<Event> e; e.reserve(n * (n - 1) / 2);
+  for (int i = 0; i < n; i++)
+    for (int j = i + 1; j < n; j++)
+      e.emplace_back(makePositive(p[i] - p[j]), i, j);
+  sort(all(e));
+  vector<int> ord(n), pos(n);
+  iota(all(ord), 0);
+  sort(all(ord), [&p](int i, int j) {
+    return cmpxy(p[i], p[j]); });
+  for (int i = 0; i < n; i++) pos[ord[i]] = i;
+  const auto makeReverse = [](auto &v) {
+    sort(all(v)); v.erase(unique(all(v)), v.end());
+    vector<pair<int,int>> segs;
+    for (size_t i = 0, j = 0; i < v.size(); i = j) {
+      for (; j < v.size() && v[j] - v[i] <= j - i; j++);
+      segs.emplace_back(v[i], v[j - 1] + 1 + 1);
+    }
+    return segs;
+  };
+  for (size_t i = 0, j = 0; i < e.size(); i = j) {
+    /* do here */
+    vector<size_t> tmp;
+    for (; j < e.size() && !(e[i] < e[j]); j++)
+      tmp.push_back(min(pos[e[j].u], pos[e[j].v]));
+    for (auto [l, r] : makeReverse(tmp)) {
+      reverse(ord.begin() + l, ord.begin() + r);
+      for (int t = l; t < r; t++) pos[ord[t]] = t;
+    }
+  }
 }
+
