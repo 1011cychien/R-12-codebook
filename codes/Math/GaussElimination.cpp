41555a5693aa52cbd25d5b376a74941ff07b58bd
OmeletWithoutEgg <2qbingxuan@gmail.com>
2023-11-24 13:44:31 +0800

    Update GaussianElimination.cpp

--- codes/Math/GaussElimination.cpp
+++ codes/Math/GaussElimination.cpp
@@ -1,17 +1,19 @@
-void gauss(vector<vector<llf>> &d) {
-  int n = d.size(), m = d[0].size();
+void gauss(vector<vector<llf>> A, vector<llf> b) {
+  const int n = A.size(), m = A[0].size();
   for (int i = 0; i < m; ++i) {
     int p = -1;
     for (int j = i; j < n; ++j)
-      if (abs(d[j][i]) > eps)
-        if (p == -1 || abs(d[j][i]) > abs(d[p][i]))
+      if (abs(A[j][i]) > eps)
+        if (p == -1 || abs(A[j][i]) > abs(A[p][i]))
           p = j;
     if (p == -1) continue;
-    for (int j = 0; j < m; ++j) swap(d[p][j], d[i][j]);
+    swap(A[p], A[i]);
+    swap(b[p], b[i]);
     for (int j = 0; j < n; ++j) if (j != i) {
-      llf z = d[j][i] / d[i][i];
+      llf z = A[j][i] / A[i][i];
       for (int k = 0; k < m; ++k)
-        d[j][k] -= z * d[i][k];
+        A[j][k] -= z * A[i][k];
+      b[j] -= z * b[i];
     }
   }
 }
