1abcdf6296936443fa277e598b515875c00e7fc1
OmeletWithoutEgg <2qbingxuan@gmail.com>
2023-11-30 10:20:38 +0800

    Update PointInPolyFast.cpp

--- codes/Geometry/PointInPolyFast.cpp
+++ codes/Geometry/PointInPolyFast.cpp
@@ -1,102 +1,69 @@
-/// source: 2021 YCPC
-
-using llf = long double;
-using P = complex<llf>;
-
-int n, m;
-vector<P> poly;
-vector<P> query;
-vector<int> ans;
-
-struct Segment {
-  P a, b;
-  int id;
-};
-vector<Segment> segs;
-
-llf Xnow;
-inline llf get_y(const Segment &u, llf xnow = Xnow) {
-  const P &a = u.a;
-  const P &b = u.b;
-  return (a.y * (b.x - xnow) + b.y * (xnow - a.x)) /
-         (b.x - a.x);
-}
-bool operator<(Segment u, Segment v) {
-  llf yu = get_y(u);
-  llf yv = get_y(v);
-  if (yu != yv) return yu < yv;
-  return u.id < v.id;
-}
-ordered_map<Segment> st; // pbds tree
-
-struct Event {
-  int type; // +1 insert seg, -1 remove seg, 0 query
-  llf x, y;
-  int id;
-};
-bool operator<(Event a, Event b) {
-  if (a.x != b.x) return a.x < b.x;
-  if (a.type != b.type) return a.type < b.type;
-  return a.y < b.y;
-}
-vector<Event> events;
-
-void solve() {
-  set<llf> xs;
-  set<P, cmpxy> ps;
-  for (int i = 0; i < n; i++) {
-    xs.insert(poly[i].x);
-    ps.insert(poly[i]);
+vector<int> PIPfast(vector<P> p, vector<P> q) {
+  const int N = int(p.size()), Q = int(q.size());
+  vector<pair<P, int>> evt;
+  vector<Seg> edge;
+  for (int i = 0; i < N; i++) {
+    int a = i, b = (i + 1) % N;
+    P A = p[a], B = p[b];
+    assert (A < B || B < A); // std::operator<
+    if (B < A) swap(A, B);
+    evt.emplace_back(A, i);
+    evt.emplace_back(B, ~i);
+    edge.emplace_back(A, B);
   }
-  for (int i = 0; i < n; i++) {
-    Segment s{poly[i], poly[(i + 1) % n], i};
-    if (s.a.x > s.b.x ||
-        (s.a.x == s.b.x && s.a.y > s.b.y))
-      swap(s.a, s.b);
-    segs.push_back(s);
-    if (s.a.x != s.b.x) {
-      events.push_back({+1, s.a.x + 0.2, s.a.y, i});
-      events.push_back({-1, s.b.x - 0.2, s.b.y, i});
+  for (int i = 0; i < Q; i++)
+    evt.emplace_back(q[i], i + N);
+  sort(all(evt));
+  auto vtx = p; sort(all(vtx));
+  auto eval = [](const Seg &a, lld x) -> llf {
+    if (RE(a.dir) == 0) {
+      assert (x == RE(a.st));
+      return IM(a.st) + llf(IM(a.dir)) / 2;
     }
-  }
-  for (int i = 0; i < m; i++) {
-    events.push_back({0, query[i].x, query[i].y, i});
-  }
-  sort(events.begin(), events.end());
-  int cnt = 0;
-  for (Event e : events) {
-    int i = e.id;
-    Xnow = e.x;
-    if (e.type == 0) {
-      llf x = e.x;
-      llf y = e.y;
-      Segment tmp = {{x - 1, y}, {x + 1, y}, -1};
-      auto it = st.lower_bound(tmp);
-
-      if (ps.count(query[i]) > 0) {
-        ans[i] = 0;
-      } else if (xs.count(x) > 0) {
-        ans[i] = -2;
-      } else if (it != st.end() &&
-                 get_y(*it) == get_y(tmp)) {
-        ans[i] = 0;
-      } else if (it != st.begin() &&
-                 get_y(*prev(it)) == get_y(tmp)) {
-        ans[i] = 0;
-      } else {
-        int rk = st.order_of_key(tmp);
-        if (rk % 2 == 1) {
-          ans[i] = 1;
-        } else {
-          ans[i] = -1;
-        }
+    llf t = (x - RE(a.st)) / llf(RE(a.dir));
+    return IM(a.st) + IM(a.dir) * t;
+  };
+  lld cur_x = 0;
+  auto cmp = [&](const Seg &a, const Seg &b) -> bool {
+    if (int s = sgn(eval(a, cur_x) - eval(b, cur_x)))
+      return s == -1;
+    int s = sgn(cross(b.dir, a.dir));
+    if (cur_x != RE(a.st) && cur_x != RE(b.st)) s *= -1;
+    return s == -1;
+  };
+  namespace pbds = __gnu_pbds;
+  using Tree = pbds::tree<Seg, int, decltype(cmp),
+        pbds::rb_tree_tag,
+        pbds::tree_order_statistics_node_update>;
+  Tree st(cmp);
+  vector<int> ans(Q);
+  for (auto [ep, i] : evt) {
+    cur_x = RE(ep);
+    if (i < 0) { // remove
+      st.erase(edge[~i]);
+    } else if (i < N) { // insert
+      auto [it, succ] = st.insert({edge[i], i});
+      assert (succ);
+    } else {
+      int qid = i - N;
+      if (binary_search(all(vtx), ep)) { // on vertex
+        ans[qid] = 1;
+        continue;
       }
-    } else if (e.type == 1) {
-      st.insert(segs[i]);
-      assert((int)st.size() == ++cnt);
-    } else if (e.type == -1) {
-      st.erase(segs[i]);
-      assert((int)st.size() == --cnt);
+      Seg H(ep, ep); // ??
+      auto it = st.lower_bound(H);
+      if (it != st.end() && isInter(it->first, ep)) {
+        ans[qid] = 1; // on edge
+        continue;
+      }
+      if (it != st.begin() && isInter(prev(it)->first, ep)) {
+        ans[qid] = 1; // on edge
+        continue;
+      }
+      auto rk = st.order_of_key(H);
+      if (rk % 2 == 0) ans[qid] = 0; // outside
+      else ans[qid] = 2; // inside
     }
   }
-}
+  return ans;
+} // test @ AOJ CGL_3_C
