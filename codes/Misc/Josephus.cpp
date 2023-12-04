cb6f28f49e228158103f9cf1cc8f5565947f1187
OmeletWithoutEgg <2qbingxuan@gmail.com>
2023-12-04 17:47:34 +0800

    Update and test Josephus.cpp

--- codes/Misc/Josephus.cpp
+++ codes/Misc/Josephus.cpp
@@ -1,10 +1,10 @@
-int f(int n, int m) { // n people kill m for each turn
-  int s = 0;
-  for (int i = 2; i <= n; i++) s = (s + m) % i;
+lld f(lld n, lld m, lld k) { // n people kill m for each turn
+  lld s = (m - 1) % (n - k); // O(k)
+  for (lld i = n - k + 1; i <= n; i++) s = (s + m) % i;
   return s;
 }
-int kth(int n, int m, int k) { // died at kth
-  if (m == 1) return n - 1;
+lld kth(lld n, lld m, i128 k) { // died at kth
+  if (m == 1) return k;         // O(m log(n))
   for (k = k*m+m-1; k >= n; k = k-n + (k-n)/(m-1));
   return k;
-}
+} // k and result are 0-based, test @ CF 101955
