3d3870f830803774844c36d1c2ff56dd98020c77
OmeletWithoutEgg <2qbingxuan@gmail.com>
2024-01-11 04:48:36 +0800

    Update TreeHash.cpp so that it can be copy-paste verified

--- codes/Graph/TreeHash.cpp
+++ codes/Graph/TreeHash.cpp
@@ -1,9 +1,10 @@
+vector<int> g[maxn]; llu h[maxn];
 llu F(llu z) { // xorshift64star from iwiwi
   z ^= z >> 12; z ^= z << 25; z ^= z >> 27;
   return z * 2685821657736338717LL;
 }
 llu hsah(int u, int f) {
   llu r = 127; // bigger?
-  for (int v : G[u]) if (v != f) r += F( hsah(v, u) );
-  return F(r);
-} // test @ UOJ 763
+  for (int v : g[u]) if (v != f) r += hsah(v, u);
+  return h[u] = F(r);
+} // test @ UOJ 763 & yosupo library checker
