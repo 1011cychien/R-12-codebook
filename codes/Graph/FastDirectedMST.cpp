b653b23a397c68061965576b6315e7815db0c114
OmeletWithoutEgg <2qbingxuan@gmail.com>
2024-01-10 20:33:56 +0800

    Update DMST and RoundSquareTree

--- codes/Graph/FastDirectedMST.cpp
+++ codes/Graph/FastDirectedMST.cpp
@@ -1,39 +1,42 @@
-using D = int64_t;
-struct E { int s, t; D w; }; // 0-base
+using lld = int64_t;
+struct E { int s, t; lld w; }; // 0-base
+struct PQ {
+  struct P {
+    lld v; int i;
+    bool operator>(const P &b) const { return v > b.v; }
+  };
+  min_heap<P> pq; lld tag;
+  void push(P p) { p.v -= tag; pq.emplace(p); }
+  P top() { P p = pq.top(); p.v += tag; return p; }
+  void join(PQ &b) {
+    if (pq.size() < b.pq.size())
+      swap(pq, b.pq), swap(tag, b.tag);
+    while (!b.pq.empty()) push(b.top()), b.pq.pop();
+  }
+};
 vector<int> dmst(const vector<E> &e, int n, int root) {
-  using PQ = pair<min_heap<pair<D, int>>, D>;
-  auto push = [](PQ &pq, pair<D, int> v) {
-    pq.first.emplace(v.first - pq.second, v.second); };
-  auto top = [](const PQ &pq) -> pair<D, int> {
-    auto r = pq.first.top();
-    return {r.first + pq.second, r.second}; };
-  auto join = [&push, &top](PQ &a, PQ &b) {
-    if (a.first.size() < b.first.size()) swap(a, b);
-    for (; !b.first.empty(); b.first.pop())
-      push(a, top(b)); };
   vector<PQ> h(n * 2);
-  for (size_t i = 0; i < e.size(); ++i)
-    push(h[e[i].t], {e[i].w, i});
-  vector<int> a(n*2), v(n*2, -1), pa(n*2, -1), r(n*2);
-  iota(a.begin(), a.end(), 0);
-  auto o = [&](int x) { int y;
-    for (y = x; a[y] != y; y = a[y]);
-    for (int ox = x; x != y; ox = x)
-      x = a[x], a[ox] = y;
-    return y; };
-  int pc = (v[root] = n + 1) - 1;
+  for (int i = 0; i < int(e.size()); ++i)
+    h[e[i].t].push({e[i].w, i});
+  vector<int> a(n * 2); iota(all(a), 0);
+  vector<int> v(n * 2, -1), pa(n * 2, -1), r(n * 2);
+  auto o = [&](auto Y, int x) -> int {
+    return x==a[x] ? x : a[x] = Y(Y, a[x]); };
+  auto S = [&](int i) { return o(o, e[i].s); };
+  int pc = v[root] = n;
   for (int i = 0; i < n; ++i) if (v[i] == -1)
-    for (int p=i; v[p]<0||v[p]==i; p=o(e[r[p]].s)) {
-      if (int q = p; v[q] == i && (p = pc++, 1)) do {
-        h[q].second = -h[q].first.top().first;
-        join(h[pa[q] = a[q] = p], h[q]);
-      } while ((q = o(e[r[q]].s)) != p);
-      for(v[p]=i;!h[p].first.empty()&&o(e[top(h[p]).second].s)==p;h[p].first.pop());
-      r[p] = top(h[p]).second;
+    for (int p = i; v[p]<0 || v[p]==i; p = S(r[p])) {
+      if (v[p] == i)
+        for (int q = pc++; p != q; p = S(r[p])) {
+          h[p].tag -= h[p].top().v; h[q].join(h[p]);
+          pa[p] = a[p] = q;
+        }
+      while (S(h[p].top().i) == p) h[p].pq.pop();
+      v[p] = i; r[p] = h[p].top().i;
     }
   vector<int> ans;
-  for (int i=pc-1;i>=0;i--) if (i!=root&&v[i]!=n) {
-    for (int f = e[r[i]].t; f!=-1&&v[f]!=n; f = pa[f])
+  for (int i = pc - 1; i >= 0; i--) if (v[i] != n) {
+    for (int f = e[r[i]].t; f != -1 && v[f] != n; f = pa[f])
       v[f] = n;
     ans.push_back(r[i]);
   }
--- codes/Graph/RoundSquareTree.cpp
+++ codes/Graph/RoundSquareTree.cpp
@@ -1,20 +1,20 @@
-struct RST {
+struct RST { // be careful about isolate point
   int n; vector<vector<int>> T;
-  RST(auto &G) : n(G.size()), T(n) {
+  RST(auto &G) : n(int(G.size())), T(n) {
     vector<int> stk, vis(n), low(n);
     auto dfs = [&](auto self, int u, int d) -> void {
       low[u] = vis[u] = d; stk.push_back(u);
       for (int v : G[u]) if (!vis[v]) {
         self(self, v, d + 1);
         if (low[v] == vis[u]) {
-          int cnt = T.size(); T.emplace_back();
+          int cnt = int(T.size()); T.emplace_back();
           for (int x = -1; x != v; stk.pop_back())
             T[cnt].push_back(x = stk.back());
           T[u].push_back(cnt); // T is rooted
         } else low[u] = min(low[u], low[v]);
       } else low[u] = min(low[u], vis[v]);
     };
-    for (int u = 0; u < N; u++)
+    for (int u = 0; u < n; u++)
       if (!vis[u]) dfs(dfs, u, 1);
   } // T may be forest; after dfs, stk are the roots
 }; // test @ 2020 Shanghai K
