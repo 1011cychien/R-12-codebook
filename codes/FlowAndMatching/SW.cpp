5b1b96024a563699b3437485ad0329145b38bf89
OmeletWithoutEgg <2qbingxuan@gmail.com>
2024-01-14 05:58:23 +0800

    Update SW.cpp

--- codes/FlowAndMatching/SW.cpp
+++ codes/FlowAndMatching/SW.cpp
@@ -1,37 +1,25 @@
-const int maxn = 500 + 5;
-int w[maxn][maxn], g[maxn];
-bool v[maxn], del[maxn];
-void add_edge(int x, int y, int c) {
-  w[x][y] += c; w[y][x] += c;
-}
-pair<int, int> phase(int n) {
-  memset(v, false, sizeof(v));
-  memset(g, 0, sizeof(g));
-  int s = -1, t = -1;
-  while (true) {
+void add_edge(auto &w, int u, int v, int c) {
+  w[u][v] += c; w[v][u] += c; }
+auto phase(const auto &w, int n, vector<int> id) {
+  vector<lld> g(n); int s = -1, t = -1;
+  while (!id.empty()) {
     int c = -1;
-    for (int i = 0; i < n; ++i) {
-      if (del[i] || v[i]) continue;
-      if (c == -1 || g[i] > g[c]) c = i;
-    }
-    if (c == -1) break;
-    v[s = t, t = c] = true;
-    for (int i = 0; i < n; ++i) {
-      if (del[i] || v[i]) continue;
-      g[i] += w[c][i];
-    }
+    for (int i : id) if (c == -1 || g[i] > g[c]) c = i;
+    s = t; t = c;
+    id.erase(ranges::find(id, c));
+    for (int i : id) g[i] += w[c][i];
   }
-  return make_pair(s, t);
+  return tuple{s, t, g[t]};
 }
-int mincut(int n) {
-  int cut = 1e9;
-  memset(del, false, sizeof(del));
+lld mincut(auto w, int n) {
+  lld cut = numeric_limits<lld>::max();
+  vector<int> id(n); iota(all(id), 0);
   for (int i = 0; i < n - 1; ++i) {
-    int s, t; tie(s, t) = phase(n);
-    del[t] = true; cut = min(cut, g[t]);
-    for (int j = 0; j < n; ++j) {
-      w[s][j] += w[t][j]; w[j][s] += w[j][t];
-    }
+    auto [s, t, gt] = phase(w, n, id);
+    id.erase(ranges::find(id, t));
+    cut = min(cut, gt);
+    for (int j = 0; j < n; ++j)
+      w[s][j] += w[t][j], w[j][s] += w[j][t];
   }
   return cut;
-}
+} // O(V^3), can be O(VE + V^2 log V)?
