a7b3f128c0320a4720f0c7eda179a5aa62b3d108
OmeletWithoutEgg <2qbingxuan@gmail.com>
2024-01-15 07:12:32 +0800

    Update hlpp and its docs

--- codes/FlowAndMatching/hlpp.cpp
+++ codes/FlowAndMatching/hlpp.cpp
@@ -1,76 +1,53 @@
-template <int maxn, typename T = int>
-struct HLPP {
-  const T INF = numeric_limits<T>::max();
-  struct E { int to, rev; T f; };
-  int n; vector<E> G[maxn]; max_heap<pair<int,int>> pq;
-  T excess[maxn]; size_t arc[maxn];
-  int nxt[2 * maxn], prv[2 * maxn], h[maxn], mxgap;
-  HLPP(const vector<int> &deg) {
-    n = (int)deg.size(); assert(n <= maxn);
-    for (int i = 0; i < n; ++i) G[i].reserve(deg[i]);
+template <typename T> struct HLPP {
+  struct Edge { int to, rev; T flow, cap; };
+  int n, mx; vector<vector<Edge>> adj; vector<T> excess;
+  vector<int> d, cnt, active; vector<vector<int>> B;
+  void add_edge(int u, int v, int f) {
+    Edge a{v, (int)size(adj[v]), 0, f};
+    Edge b{u, (int)size(adj[u]), 0, 0};
+    adj[u].push_back(a), adj[v].push_back(b);
   }
-  void add_edge(int from, int to, int f, bool isDirected = true) {
-    G[from].emplace_back(to, int(G[to].size()), f);
-    G[to].emplace_back(from, int(G[from].size()) - 1, isDirected ? 0 : f);
-  }
-  void update_h(int v, int nh) {
-    if (h[v] != n)
-      nxt[prv[v]] = nxt[v], prv[nxt[v]] = prv[v];
-    h[v] = nh;
-    if (nh == n) return;
-    mxgap = max(mxgap, nh);
-    if (excess[v] > 0) pq.emplace(nh, v);
-    nxt[v] = nxt[nh + n]; prv[v] = nh + n;
-    nxt[nh + n] = v; prv[nxt[v]] = v;
-  }
-  void bfs(int t) {
-    for (int i = 0; i < n; ++i)
-      h[i] = n, nxt[i] = prv[i] = i;
-    vector<int> que = {t}; h[t] = 0;
-    for (size_t i = 0; i < que.size(); ++i)
-      for (int v = que[i]; auto &e : G[v])
-        if (h[e.to] == n && G[e.to][e.rev].f > 0)
-          que.push_back(e.to), update_h(e.to, h[v] + 1);
-    mxgap = h[que.back()];
-  }
-  void push(int v, E &e) {
-    T df = min(excess[v], e.f);
-    if (df == 0) return;
-    if (excess[e.to] == 0) pq.emplace(h[e.to], e.to);
-    e.f -= df, G[e.to][e.rev].f += df;
-    excess[v] -= df, excess[e.to] += df;
-  }
-  bool discharge(int v) {
-    int nh = n;
-    for (size_t j = 0, S = G[v].size(); j < S; ++j) {
-      auto i = j+arc[v]>=S ? j+arc[v]-S : j+arc[v];
-      auto &e = G[v][i];
-      if (e.f == 0) continue;
-      if (h[v] == h[e.to] + 1) {
-        push(v, e);
-        if (excess[v] <= 0) return arc[v] = i, false;
-      } else nh = min(nh, h[e.to] + 1);
+  void enqueue(int v) {
+    if (!active[v] && excess[v] > 0 && d[v] < n) {
+      mx = max(mx, d[v]);
+      B[d[v]].push_back(v); active[v] = 1;
     }
-    if (nxt[h[v] + n] != h[v] + n) update_h(v, nh);
-    else
-      for (int oldh = h[v], &g = mxgap; g >= oldh; --g) {
-        for (int i = nxt[g + n]; i < n; i = nxt[i])
-          h[i] = n;
-        nxt[g + n] = prv[g + n] = g + n;
-      }
-    return true;
+  }
+  void push(int v, Edge &e) {
+    T df = min(excess[v], e.cap - e.flow);
+    if (df <= 0 || d[v] != d[e.to] + 1) return;
+    e.flow += df, adj[e.to][e.rev].flow -= df;
+    excess[e.to] += df, excess[v] -= df;
+    enqueue(e.to);
+  }
+  void gap(int k) {
+    for (int v = 0; v < n; v++) if (d[v] >= k)
+      cnt[d[v]]--, d[v] = n, cnt[d[v]]++;
+  }
+  void relabel(int v) {
+    cnt[d[v]]--; d[v] = n;
+    for (auto e : adj[v])
+      if (e.cap > e.flow) d[v] = min(d[v], d[e.to] + 1);
+    cnt[d[v]]++; enqueue(v);
+  }
+  void discharge(int v) {
+    for (auto &e : adj[v])
+      if (excess[v] > 0) push(v, e);
+      else break;
+    if (excess[v] <= 0) return;
+    if (cnt[d[v]] == 1) gap(d[v]);
+    else relabel(v);
   }
   T max_flow(int s, int t) {
-    fill(arc, arc + n, 0); fill(excess, excess + n, 0);
-    excess[s] = INF; excess[t] = -INF;
-    bfs(t); for (auto &e : G[s]) push(s, e);
-    int work = 0;
-    while (!pq.empty()) {
-      auto [hv, v] = pq.top(); pq.pop();
-      if (h[v] != hv) continue;
-      work += discharge(v);
-      if (work > 4 * n) bfs(t), work = 0;
-    }
-    return excess[t] + INF;
+    for (auto &e : adj[s]) excess[s] += e.cap;
+    cnt[0] = n; enqueue(s); active[t] = 1;
+    for (mx = 0; mx >= 0;)
+      if (!B[mx].empty()) {
+        int v = B[mx].back(); B[mx].pop_back();
+        active[v] = 0; discharge(v);
+      } else --mx;
+    return excess[t];
   }
+  HLPP(int _n) : n(_n), adj(n), excess(n),
+    d(n), cnt(n + 1), active(n), B(n) {}
 };
