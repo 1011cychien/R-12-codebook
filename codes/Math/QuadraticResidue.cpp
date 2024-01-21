721489798592861d24aadfd9ef8a206b878af063
OmeletWithoutEgg <2qbingxuan@gmail.com>
2024-01-22 00:03:11 +0800

    Fix warning in QuadraticResidue.cpp

--- codes/Math/QuadraticResidue.cpp
+++ codes/Math/QuadraticResidue.cpp
@@ -1,7 +1,7 @@
 int get_root(int n, int P) { // ensure 0 <= n < p
   if (P == 2 or n == 0) return n;
-  auto check = [&](int x) {
-    return modpow(x, (P - 1) / 2, P); };
+  auto check = [&](lld x) {
+    return modpow(int(x), (P - 1) / 2, P); };
   if (check(n) != 1) return -1;
   mt19937 rnd(7122); lld z = 1, w;
   while (check(w = (z * z - n + P) % P) != P - 1)
@@ -12,7 +12,7 @@ int get_root(int n, int P) { // ensure 0 <= n < p
         (a * d + b * c) % P);
   };
   pair<lld, lld> r(1, 0), e(z, 1);
-  for (int w = (P + 1) / 2; w; w >>= 1, e = M(e, e))
-    if (w & 1) r = M(r, e);
-  return r.first; // sqrt(n) mod P where P is prime
+  for (int q = (P + 1) / 2; q; q >>= 1, e = M(e, e))
+    if (q & 1) r = M(r, e);
+  return int(r.first); // sqrt(n) mod P where P is prime
 }
