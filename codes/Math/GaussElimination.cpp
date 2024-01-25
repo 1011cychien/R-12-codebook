5f3d34bdf52927fdf25a01417a70540466c9f845
OmeletWithoutEgg <2qbingxuan@gmail.com>
2024-01-25 19:08:15 +0800

    Put a complete gaussian elimination

--- codes/Math/GaussElimination.cpp
+++ codes/Math/GaussElimination.cpp
@@ -1,19 +1,34 @@
-void gauss(vector<vector<llf>> A, vector<llf> b) {
-  const int n = A.size(), m = A[0].size();
-  for (int i = 0; i < m; ++i) {
+using VI = vector<int>;
+using VVI = vector<VI>;
+pair<VI, VVI> gauss(VVI &A, VI &b) {
+  const int N = (int)A.size(), M = (int)A[0].size();
+  vector<int> depv, free(M, true); int rk = 0;
+  for (int i = 0; i < M; ++i) {
     int p = -1;
-    for (int j = i; j < n; ++j)
-      if (abs(A[j][i]) > eps)
-        if (p == -1 || abs(A[j][i]) > abs(A[p][i]))
-          p = j;
-    if (p == -1) continue;
-    swap(A[p], A[i]);
-    swap(b[p], b[i]);
-    for (int j = 0; j < n; ++j) if (j != i) {
-      llf z = A[j][i] / A[i][i];
-      for (int k = 0; k < m; ++k)
-        A[j][k] -= z * A[i][k];
-      b[j] -= z * b[i];
+    for (int j = rk; j < N; ++j)
+      if (p == -1 || abs(A[j][i]) > abs(A[p][i]))
+        p = j;
+    if (p == -1 || A[p][i] == 0) continue;
+    swap(A[p], A[rk]); swap(b[p], b[rk]);
+    const int inv = modinv(A[rk][i]);
+    for (int &x : A[rk]) x = mul(x, inv);
+    b[rk] = mul(b[rk], inv);
+    for (int j = 0; j < N; ++j) if (j != rk) {
+      int z = A[j][i];
+      for (int k = 0; k < M; ++k)
+        A[j][k] = sub(A[j][k], mul(z, A[rk][k]));
+      b[j] = sub(b[j], mul(z, b[rk]));
     }
+    depv.push_back(i); free[i] = false; ++rk;
   }
+  for (int i = rk; i < N; i++)
+    if (b[i] != 0) return {{}, {}}; // not consistent
+  VI x(M); VVI h;
+  for (int i = 0; i < rk; i++) x[depv[i]] = b[i];
+  for (int i = 0; i < M; i++) if (free[i]) {
+    h.emplace_back(M); h.back()[i] = 1;
+    for (int j = 0; j < rk; j++)
+      h.back()[depv[j]] = sub(0, A[j][i]);
+  }
+  return {x, h}; // solution = x + span(h[i])
 }
