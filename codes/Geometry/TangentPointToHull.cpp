0de4fca7fa466ea1381201478e332886b29827cf
OmeletWithoutEgg <2qbingxuan@gmail.com>
2024-02-04 05:21:24 +0800

    Fix CE

--- codes/Geometry/TangentPointToHull.cpp
+++ codes/Geometry/TangentPointToHull.cpp
@@ -1,9 +1,9 @@
 pair<int, int> get_tangent(const vector<P> &v, P p) {
   auto gao = [&](int s) {
-    return cyclic_ternary_search([&](int x, int y) {
-      return ori(p, v[x], v[y]) == s;
-    });
+    return cyclic_ternary_search(v.size(),
+        [&](int x, int y) {
+          return ori(p, v[x], v[y]) == s; });
   }; // test @ codeforces.com/gym/101201/problem/E
-  return {gao(-1), gao(1)}; // (a,b):ori(p,v[a],v[b])<0
+  return {gao(1), gao(-1)}; // (a,b):ori(p,v[a],v[b])<0
 } // plz ensure that point strictly out of hull
     // if colinear, returns arbitrary point on line
