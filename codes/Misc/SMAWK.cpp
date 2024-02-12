be19965f2bfc9b740be8ffece8d2ec9eff93f9c5
OmeletWithoutEgg <2qbingxuan@gmail.com>
2024-02-13 00:04:53 +0800

    Fix SMAWK CE and shorten it

--- codes/Misc/SMAWK.cpp
+++ codes/Misc/SMAWK.cpp
@@ -3,33 +3,27 @@
 // If M[1][0] == M[1][1], M[0][0] <= M[0][1]
 // M[i][ans_i] is the best value in the i-th row
 VI smawk(int N, int M, auto &&select) {
-  auto solve = [&](const VI &r, const VI &c) {
+  auto dc = [&](auto self, const VI &r, const VI &c) -> VI {
     if (r.empty()) return {};
-    const int n = (int)r.size(); vector<int> c2;
+    const int n = (int)r.size(); VI ans(n), nr, nc;
     for (int i : c) {
-      while (!c2.empty() && select(r[c2.size() - 1], c2.back(), i))
-        c2.pop_back();
-      if (c2.size() < n) c2.push_back(i);
+      while (!nc.empty() && select(r[nc.size() - 1], nc.back(), i))
+        nc.pop_back();
+      if (int(nc.size()) < n) nc.push_back(i);
     }
-    vector<int> r2;
-    for (int i = 1; i < n; i += 2) r2.push_back(r[i]);
-    const auto a2 = solve(r2, c2);
-    vector<int> ans(n);
-    for (size_t i = 0; i < a2.size(); i++)
-      ans[i * 2 + 1] = a2[i];
+    for (int i = 1; i < n; i += 2) nr.push_back(r[i]);
+    const auto na = self(self, nr, nc);
+    for (int i = 1; i < n; i += 2) ans[i] = na[i >> 1];
     for (int i = 0, j = 0; i < n; i += 2) {
-      ans[i] = c2[j];
-      const int end = i + 1 == n ? c2.back() : ans[i + 1];
-      while (c2[j] != end) {
-        j++;
-        if (select(r[i], ans[i], c2[j])) ans[i] = c2[j];
-      }
+      ans[i] = nc[j];
+      const int end = i + 1 == n ? nc.back() : ans[i + 1];
+      while (nc[j] != end)
+        if (select(r[i], ans[i], nc[++j])) ans[i] = nc[j];
     }
     return ans;
   };
-  vector<int> row(N), col(M);
-  iota(all(row), 0), iota(all(col), 0);
-  return dc(dc, row, col);
+  VI R(N), C(M); iota(all(R), 0), iota(all(C), 0);
+  return dc(dc, R, C);
 }
 // if f(r, v) is better than f(r, v), return true
 bool min_plus_conv_select(int r, int u, int v) {
