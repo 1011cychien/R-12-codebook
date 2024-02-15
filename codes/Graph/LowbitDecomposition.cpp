376a69c1d24198c61742db669173d12876097e32
OmeletWithoutEgg <2qbingxuan@gmail.com>
2024-02-15 23:24:21 +0800

    Update LowbitDecomposition.cpp

--- codes/Graph/LowbitDecomposition.cpp
+++ codes/Graph/LowbitDecomposition.cpp
@@ -1,10 +1,10 @@
 class LBD {
-  int timer, chains;
+  int n, timer, chains;
   vector<vector<int>> G;
-  vector<int> tl, tr, chain, head, dep, pa;
+  vector<int> tl, tr, chain, top, dep, pa;
   // chains : number of chain
   // tl, tr[u] : subtree interval in the seq. of u
-  // head[i] : head of the chain i
+  // top[i] : top of the chain of vertex i
   // chian[u] : chain id of the chain u is on
   void predfs(int u, int f) {
     dep[u] = dep[pa[u] = f] + 1;
@@ -15,30 +15,27 @@ class LBD {
     }
     if (chain[u] == 0) chain[u] = ++chains;
   }
-  void dfschain(int u, int f) {
-    tl[u] = timer++;
-    if (head[chain[u]] == -1)
-      head[chain[u]] = u;
+  void dfschain(int u, int f, int t) {
+    tl[u] = timer++; top[u] = t;
     for (int v : G[u])
       if (v != f and chain[v] == chain[u])
-        dfschain(v, u);
+        dfschain(v, u, t);
     for (int v : G[u])
       if (v != f and chain[v] != chain[u])
-        dfschain(v, u);
+        dfschain(v, u, v);
     tr[u] = timer;
   }
 public:
   LBD(auto &&G_) : n((int)size(G_)),
     timer(0), chains(0), G(G_), tl(n), tr(n),
-      chain(n), head(n + 1, -1), dep(n), pa(n)
-    { predfs(0, 0); dfschain(0, 0); }
+      chain(n), top(n + 1, -1), dep(n), pa(n)
+    { predfs(0, 0); dfschain(0, 0, 0); }
   PII get_subtree(int u) { return {tl[u], tr[u]}; }
   vector<PII> get_path(int u, int v) {
     vector<PII> res;
     while (chain[u] != chain[v]) {
-      if (dep[head[chain[u]]] < dep[head[chain[v]]])
-        swap(u, v);
-      int s = head[chain[u]];
+      if (dep[top[u]] < dep[top[v]]) swap(u, v);
+      int s = top[u];
       res.emplace_back(tl[s], tl[u] + 1);
       u = pa[s];
     }
