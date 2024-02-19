0a2d40241938fe1c652b631a27d13c74c7bd0ef6
OmeletWithoutEgg <2qbingxuan@gmail.com>
2024-02-19 15:59:45 +0800

    Update BinarySearchOnFraction.cpp

--- codes/Misc/BinarySearchOnFraction.cpp
+++ codes/Misc/BinarySearchOnFraction.cpp
@@ -1,17 +1,16 @@
 struct Q {
-  ll p, q;
-  Q go(Q b, ll d) { return {p + b.p*d, q + b.q*d}; }
+  lld p, q; // p / q
+  Q go(Q b, lld d) { return {p + b.p*d, q + b.q*d}; }
 };
-bool pred(Q);
 // returns smallest p/q in [lo, hi] such that
 // pred(p/q) is true, and 0 <= p,q <= N
-Q frac_bs(ll N) {
+Q frac_bs(lld N, auto &&pred) {
   Q lo{0, 1}, hi{1, 0};
   if (pred(lo)) return lo;
   assert(pred(hi));
   bool dir = 1, L = 1, H = 1;
   for (; L || H; dir = !dir) {
-    ll len = 0, step = 1;
+    lld len = 0, step = 1;
     for (int t = 0; t < 2 && (t ? step/=2 : step*=2);)
       if (Q mid = hi.go(lo, len + step);
           mid.p > N || mid.q > N || dir ^ pred(mid))
@@ -22,4 +21,3 @@ Q frac_bs(ll N) {
   }
   return dir ? hi : lo;
 }
-
