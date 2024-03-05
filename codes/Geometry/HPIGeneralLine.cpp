b5874873863229c6d1692711137f681031dc93da
OmeletWithoutEgg <2qbingxuan@gmail.com>
2024-03-05 17:01:59 +0800

    Fix HPIGeneralLine.cpp

--- codes/Geometry/HPIGeneralLine.cpp
+++ codes/Geometry/HPIGeneralLine.cpp
@@ -1,18 +1,23 @@
 struct Line {
   lld a, b, c; // ax + by + c <= 0
   P dir() const { return P(a, b); }
+  Line(lld ta, lld tb, lld tc) : a(ta), b(tb), c(tc) {}
+  Line(P S, P T):a(IM(T-S)),b(-RE(T-S)),c(cross(T,S)) {}
 }; using LN = const Line &;
 PF intersect(LN A, LN B) {
-  P x(A.a, B.a), y(A.b, B.b), z(A.c, B.c);
-  llf D = cross(x, y);
-  return PF(cross(y, z) / D, cross(z, x) / D);
+  llf c = cross(A.dir(), B.dir());
+  i128 a = i128(A.c) * B.a - i128(B.c) * A.a;
+  i128 b = i128(A.c) * B.b - i128(B.c) * A.b;
+  return PF(-b / c, a / c);
 }
 bool cov(LN l, LN A, LN B) {
-  P x(A.a, B.a), y(A.b, B.b), z(A.c, B.c);
-  i128 c = cross(x,y), a = cross(y,z), b = cross(z,x);
-  return sgn(a * l.a + b * l.b + c * l.c) * sgn(c) > 0;
+  i128 c = cross(A.dir(), B.dir());
+  i128 a = i128(A.c) * B.a - i128(B.c) * A.a;
+  i128 b = i128(A.c) * B.b - i128(B.c) * A.b;
+  return sgn(a * l.b - b * l.a + c * l.c) * sgn(c) >= 0;
 }
 bool operator<(LN a, LN b) {
   if (int c = argCmp(a.dir(), b.dir())) return c == -1;
-  return a.c > b.c;
+  return i128(abs(b.a) + abs(b.b)) * a.c >
+                  i128(abs(a.a) + abs(a.b)) * b.c;
 }
