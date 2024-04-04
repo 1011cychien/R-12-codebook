ab87be63f655ba3f0f06cea7b34d61c08a653bde
OmeletWithoutEgg <2qbingxuan@gmail.com>
2024-04-04 23:52:01 +0800

    Update exgcd

--- codes/Math/ChineseRemainder.cpp
+++ codes/Math/ChineseRemainder.cpp
@@ -1,10 +1,10 @@
 // please ensure r_i\in[0,m_i)
 bool crt(lld &m1, lld &r1, lld m2, lld r2) {
-    if (m2 > m1) swap(m1, m2), swap(r1, r2);
-    lld g, a, b; exgcd(m1, m2, g, a, b);
-    if ((r2 - r1) % g != 0) return false;
-    m2 /= g; lld D = (r2 - r1) / g % m2 * a % m2;
-    r1 += (D < 0 ? D + m2 : D) * m1; m1 *= m2;
-    assert (r1 >= 0 && r1 < m1);
-    return true;
+  if (m2 > m1) swap(m1, m2), swap(r1, r2);
+  auto [g, a, b] = exgcd(m1, m2);
+  if ((r2 - r1) % g != 0) return false;
+  m2 /= g; lld D = (r2 - r1) / g % m2 * a % m2;
+  r1 += (D < 0 ? D + m2 : D) * m1; m1 *= m2;
+  assert (r1 >= 0 && r1 < m1);
+  return true;
 }
--- codes/Math/ExtendedGCD.cpp
+++ codes/Math/ExtendedGCD.cpp
@@ -1,5 +1,6 @@
 // ax+ny = 1, ax+ny == ax == 1 (mod n)
-void exgcd(lld x, lld y, lld &g, lld &a, lld &b) {
-  if (y == 0) g = x, a = 1, b = 0;
-  else exgcd(y, x % y, g, b, a), b -= (x / y) * a;
+tuple<lld,lld,lld> exgcd(lld x, lld y) {
+  if (y == 0) return {x, 1, 0};
+  auto [g, b, a] = exgcd(y, x % y);
+  return {g, a, b - (x / y) * a};
 }
