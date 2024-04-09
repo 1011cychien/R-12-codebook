c94ed8a817e7826f5744bce9169c97a7249c88e2
OmeletWithoutEgg <2qbingxuan@gmail.com>
2024-04-09 17:12:15 +0800

    Fix HullCut halfplane direction

--- codes/Geometry/HullCut.cpp
+++ codes/Geometry/HullCut.cpp
@@ -2,8 +2,8 @@ vector<P> cut(const vector<P> &p, P s, P e) {
   vector<P> res;
   for (size_t i = 0; i < p.size(); i++) {
     P cur = p[i], prv = i ? p[i-1] : p.back();
-    bool side = ori(s, e, cur) < 0;
-    if (side != (ori(s, e, prv) < 0))
+    bool side = ori(s, e, cur) > 0;
+    if (side != (ori(s, e, prv) > 0))
       res.push_back(intersect({s, e}, {cur, prv}));
     if (side) res.push_back(cur);
   } // P is complex<llf>
